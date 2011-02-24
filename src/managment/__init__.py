a4afb5445bcb82e4670d775ea8a72eb50e0636f4
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-02-24 17:43:23 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@6647 230e8929-42e3-3b44-a413-55659dd3a577

---
+++ src/managment/__init__.py
@@ -0,0 +1,11 @@
+
+#from management import internal_mngr
+#import managers
+#managers.reg_manager("server_manager", internal_mngr)
+#from utils.server import VDOM_server
+#VDOM_server()
+
+from manager import 
+
+from thread import VDOM_thread, VDOM_daemon
+from server import VDOM_server
