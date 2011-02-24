a1d2854f802d79af49f7f1836760daaf6c1116b2
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-02-24 09:39:16 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@6643 230e8929-42e3-3b44-a413-55659dd3a577

--- bin/vdomsvr.py
+++ bin/vdomsvr.py
@@ -4,190 +4,25 @@
 main module to run VDOM HTTP Server
 """
 
-import sys, time, os, thread, socket, gettext, shutil, stat, __builtin__
-import encodings.utf_16, encodings.utf_16_be, encodings.utf_16_le, encodings.utf_7, encodings.utf_8, encodings.utf_8_sig
-import xmlrpclib
-
-sys.path.append("../src")
-
-from config import *
-__builtin__.VDOM_CONFIG = VDOM_CONFIG
-__builtin__.VDOM_CONFIG_1 = VDOM_CONFIG_1
-
-# logger
-if sys.platform.startswith("freebsd") or sys.platform.startswith("linux"):
-	try:
-		f = open(VDOM_CONFIG["LOGGER-PIDFILE"], "r")
-		pid = f.read()
-		f.close()
-		os.system("kill " + pid)
-		time.sleep(1)
-	except: pass
-	i = os.spawnl(os.P_NOWAIT, "logger")
-	f = open(VDOM_CONFIG["LOGGER-PIDFILE"], "w")
-	f.write(str(i))
-	f.close()
-
-def send_to_log(data):
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	try:
-		s.sendto(data, ('localhost', VDOM_CONFIG["LOCALHOST-LOGGER-PORT"]))
-	except:
-		pass
-
-if sys.platform.startswith("freebsd") or sys.platform.startswith("linux"):
-	time.sleep(1)
-	send_to_log(str(VDOM_CONFIG["LOG-FILE-COUNT"]))
-	send_to_log(str(VDOM_CONFIG["LOG-FILE-SIZE"]))
-	send_to_log(VDOM_CONFIG["LOG-DIRECTORY"])
-
-sys.path.append(VDOM_CONFIG["LIB-DIRECTORY"])
-
-try: os.makedirs(VDOM_CONFIG["TYPES-LOCATION"])
-except: pass
-try: os.makedirs(VDOM_CONFIG["TEMP-DIRECTORY"] + "/types")
-except: pass
-try: os.makedirs(VDOM_CONFIG["FILE-ACCESS-DIRECTORY"] + "/applications")
-except: pass
-try: os.makedirs(VDOM_CONFIG["SOURCE-MODULES-DIRECTORY"])
-except: pass
-try: os.makedirs(VDOM_CONFIG["LOG-DIRECTORY"])
-except: pass
-try: os.makedirs(VDOM_CONFIG["BACKUP-DIRECTORY"])
-except: pass
-try: os.rmdir(VDOM_CONFIG["STORAGE-DIRECTORY"] + "/socket")
-except: pass
-try: os.makedirs(VDOM_CONFIG["STORAGE-DIRECTORY"] + "/socket")
-except: pass
-try: os.makedirs(VDOM_CONFIG["LIB-DIRECTORY"])
-except: pass
-os.chmod(VDOM_CONFIG["STORAGE-DIRECTORY"] + "/socket", stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-
-f = open(os.path.join(VDOM_CONFIG["STORAGE-DIRECTORY"], "lib", "__init__.py"), "wt")
-f.close()
-
-from storage.storage import VDOM_config
-from utils.system import console_debug
-import managers
-import log
-import utils.email1
+import __builtin__, sys, gettext, os
+#import encodings.utf_16, encodings.utf_16_be, encodings.utf_16_le, encodings.utf_7, encodings.utf_8, encodings.utf_8_sig
+#import xmlrpclib
 
 gettext.install("vdom2")
 
-# debug
-tags = managers.storage.read_object("DEBUG-TAGS")
-if not tags:
-	tags = []
-def my_debug(_data, tag="", console=None):
-	global tags
-	debug_on = True
-	if "1" != VDOM_CONFIG_1["DEBUG"]:
-		debug_on = False
-	data = ""
-	if isinstance(_data, unicode):
-		data = _data.encode("utf-8")
-	else:
-		data = str(_data)
-	if console is not None:
-		console_debug(data)
-	cf = VDOM_config()
-	if not debug_on:
-		return
-	_tag = s = ""
-	if tag:
-		_tag = tag.lower()
-		if _tag not in tags:
-			tags.append(_tag)
-			managers.storage.write_object_async("DEBUG-TAGS", tags)
-			cf.set_opt_sync("DEBUG-ENABLE-TAG-" + _tag, "1")
-	x = time.strftime("%d %b %Y %H:%M:%S", time.gmtime())
-	prep = "%s thread %4d" % (x, thread.get_ident())
-	en_tag = VDOM_CONFIG_1["DEBUG-ENABLE-TAGS"]
-	if "1" == en_tag and "" != tag and "1" == cf.get_opt("DEBUG-ENABLE-TAG-" + _tag):
-		prep += " [%s]: " % _tag
-	else:
-		prep += ": "
-	s = ("\n" + prep).join(data.split("\n"))
-	sys.stderr.write(prep + s + "\n")
-	send_to_log(prep + s + "\n")
+sys.path.append("../src")
 
-class my_debugfile:
+from config import VDOM_CONFIG, VDOM_CONFIG_1
+__builtin__.VDOM_CONFIG = VDOM_CONFIG
+__builtin__.VDOM_CONFIG_1 = VDOM_CONFIG_1
 
-	def write(self, some):
-		my_debug(some)
+sys.path.append(VDOM_CONFIG["LIB-DIRECTORY"])
 
-def increase_objects_count(app):
-	if app:
-		app.xml_manager.modify_objects_count(1)
-#	print app.xml_manager.obj_count
+import utils.console
 
-def decrease_objects_count(app):
-	if app:
-		app.xml_manager.modify_objects_count(-1)
-#	print app.xml_manager.obj_count
+# obsolete initialization code
 
-__builtin__.debug = my_debug
-__builtin__.debugfile = my_debugfile()
-__builtin__.increase_objects_count = increase_objects_count
-__builtin__.decrease_objects_count = decrease_objects_count
-__builtin__.system_options = {"server_license_type": "0",	# online server, 1=development
-				"firmware" : "N/A",
-				"card_state" : "0",	# 0 - no card, 1 - card ok
-				"object_amount" : "1500"}
-
-
-def watcher():
-	while True:
-		try:
-			data = raw_input()
-			exec data
-		except EOFError:
-			print "Keyboard interrupt"
-			#actions_on_shutdown()
-			os._exit(0)
-		except Exception, e:
-			print e
-
-# verify that server is not running
-# attempt to establish a connection on server address or localhost
-addr_try = VDOM_CONFIG["SERVER-ADDRESS"]
-if addr_try == "":
-	addr_try = "localhost"
-sock = socket.socket()
-ok = False
-try:
-	sock.connect((addr_try, VDOM_CONFIG["SERVER-PORT"]))
-except:
-	# exception means connection failed, thus server should not be running
-	ok = True
-	print "Tried", addr_try, ":", VDOM_CONFIG["SERVER-PORT"], "- no response"
-sock.close()
-if not ok:
-	print(_("Server is already running"))
-	print "Got response on", addr_try, ":", VDOM_CONFIG["SERVER-PORT"]
-	os._exit(0)
-
-#if not sys.platform.startswith("freebsd") and not sys.platform.startswith("linux") and 'WINGDB_ACTIVE' not in os.environ:
-#	thread.start_new_thread(watcher, ())
-
-# copy types
-print "Copying VDOM types to hard drive...",
-_from = VDOM_CONFIG["SOURCE-TYPES-LOCATION"]
-_to = VDOM_CONFIG["TYPES-LOCATION"]
-from_files = os.listdir(_from)
-to_files = os.listdir(_to)
-for item in from_files:
-	if "xml" != item.split(".")[-1]:
-		continue
-	if item in to_files:
-		stat_from = os.stat(_from + "/" + item)
-		stat_to = os.stat(_to + "/" + item)
-		if stat_from[8] <= stat_to[8]:
-			continue
-	shutil.copy2(_from + "/" + item, _to + "/" + item)
-if not os.path.exists(VDOM_CONFIG["APPLICATION-XML-TEMPLATE"]):
-	shutil.copy2("app_template.xml", VDOM_CONFIG["APPLICATION-XML-TEMPLATE"])
-print "Done"
+import utils.obsolete
 
 # initialize vscript engine
 
@@ -213,17 +48,19 @@ import module
 import session
 import soap
 
+import utils.email1
+
 from server.server import VDOM_box_server
 
-print "Creating VDOM server"
 server=VDOM_box_server()
 server.start()
 
-import threading
 from utils.server import VDOM_thread, VDOM_daemon
-from utils.tracing import show_active_threads, show_active_threads_stack
+from utils.tracing import show_active_threads, show_active_threads_stack, show_threads_trace
 
-show_active_threads()
+print
+#show_active_threads()
 #show_active_threads_stack()
+show_threads_trace()
 
 os._exit(0)
--- src/utils/tracing.py
+++ src/utils/tracing.py
@@ -1,6 +1,7 @@
 
 import sys, os, traceback, threading
 from server import VDOM_thread, VDOM_daemon
+import utils.console
 
 
 path_python=sys.prefix
@@ -20,40 +21,23 @@ def normalize_source_statement(statement, length=50):
 	return statement[:length-3]+"..." if len(statement)>length else statement
 
 
-def get_thread_line(thread):
-	for thread_ident, stack in sys._current_frames().items():
-		if thread_ident==thread.ident:
-			trace=traceback.extract_stack(stack)
-			path, line, function, statement=traceback.extract_stack(stack)[-1]
+def show_threads_trace(ident="", title_width=30):
+	threads={thread.ident: thread for thread in threading.enumerate()}
+	for ident, frame in sys._current_frames().items():
+		thread=threads.get(ident, None)
+		if not thread: continue
+		flags=tuple(flag for flag in ("Daemon" if thread.daemon else None, 
+			"Smart" if isinstance(thread, (VDOM_thread, VDOM_daemon)) else None) if flag)
+		details=" (%s)"%", ".join(flags) if flags else ""
+		title="%s...%s"%(thread.name[:title_width-len(details)-3], details) \
+			if len(thread.name)>title_width-len(details) else \
+			"%s%s"%(thread.name, details)
+		ident=thread.ident
+		stack=traceback.extract_stack(frame)
+		for path, line, function, statement in stack:
 			path=normalize_source_path(path)
 			statement=normalize_source_statement(statement)
-			return path, line, function, statement
+			print "%*s %6s %s:%s:%s"%(-title_width, title, ident, path, line, function)
+			title, ident="", ""
+
 	
-
-def show_active_threads(ident=""):
-	for thread in threading.enumerate():
-		path, line, function, statement=get_thread_line(thread)
-		print "%s%-20s %1s %1s %6s %s"%(ident, thread.name,
-			"D" if thread.daemon else " ",
-			"V" if isinstance(thread, (VDOM_thread, VDOM_daemon)) else " ",
-			thread.ident,
-			"%s, line %s, in %s"%(path, line, function))
-
-def show_active_threads_stack(ident=""):
-	for thread in threading.enumerate():
-		print "%s%-20s %1s %1s %6s"%(ident, thread.name,
-			"D" if thread.daemon else " ",
-			"V" if isinstance(thread, (VDOM_thread, VDOM_daemon)) else " ",
-			thread.ident)
-		path, line, function, statement=get_thread_line(thread)
-		show_thread_stack(thread, ident=ident+"    ")
-
-def show_thread_stack(thread, ident=""):
-	for thread_ident, stack in sys._current_frames().items():
-		if thread_ident!=thread.ident: continue
-		trace=traceback.extract_stack(stack)
-		for path, line, function, statement in traceback.extract_stack(stack):
-			path=normalize_source_path(path)
-			statement=normalize_source_statement(statement)
-			print "%s%s, line %s, in %s%s"%(ident, path, line, function,
-				": %s"%statement if statement else "")
