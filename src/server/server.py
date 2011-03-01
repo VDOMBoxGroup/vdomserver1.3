0379596d1ac2f015444b918ed3e42175e9b23c53
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-03-01 10:05:36 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@6659 230e8929-42e3-3b44-a413-55659dd3a577

--- src/server/server.py
+++ src/server/server.py
@@ -1,9 +1,11 @@
 
-from utils.threads import VDOM_server as VDOM_base_server
+from utils.threads import VDOM_server as VDOM_threads_server
+from memory import VDOM_xml_sync_daemon
 from web import VDOM_web_server_thread
 
 
-class VDOM_server(VDOM_base_server):
+class VDOM_server(VDOM_threads_server):
 
 	def prepare(self):
+		VDOM_xml_sync_daemon().start()
 		VDOM_web_server_thread().start()
