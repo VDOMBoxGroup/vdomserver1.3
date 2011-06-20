d6797b3ea3261a8877ec56cac3dae9060c70d89e
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-06-20 19:06:50 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@6849 230e8929-42e3-3b44-a413-55659dd3a577

--- src/scripting/__init__.py
+++ src/scripting/__init__.py
@@ -1,13 +1,17 @@
 
+from server import VDOM_server
+from application import VDOM_application
+from session import VDOM_session
 from request import VDOM_request
 from response import VDOM_response
-from application import VDOM_application
-from server import VDOM_server
-from session import VDOM_session
+
+from obsolete import VDOM_request as VDOM_obsolete
 
 
+server=VDOM_server()
+application=VDOM_application()
+session=VDOM_session()
 request=VDOM_request()
 response=VDOM_response()
-application=VDOM_application()
-server=VDOM_server()
-session=VDOM_session()
+
+obsolete=VDOM_obsolete()
