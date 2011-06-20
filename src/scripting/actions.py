df3f54b976758565610e54862d944ed8216dfccb
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-06-20 19:06:50 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@6849 230e8929-42e3-3b44-a413-55659dd3a577

---
+++ src/scripting/actions.py
@@ -0,0 +1,59 @@
+
+import thread, threading
+
+import managers
+
+import scripting # from object import request
+from vscript.engine import vcompile, vexecute
+
+
+
+class generic_action(object):
+
+	def __init__(self, id, name, source):
+		self.id=id
+		self.name=name
+		self.source=source
+		self.code=None
+
+	def compile(self):
+		debug("[Action] Compile %s action \"%s\":"%(self.id, self.name))
+
+	def on_execute(self, object, namespace):
+		pass
+
+	def execute(self, object, namespace=None):
+		application=managers.request_manager.get_request().application()
+		threading.currentThread().application=application.id
+		if self.code is None:
+			self.compile()
+		self.on_execute(object, namespace)
+		threading.currentThread().application=None
+
+class python_action(generic_action):
+
+	def compile(self):
+		generic_action.compile(self)
+		debug("- - - - - - - - - - - - - - - - - - - -\n%s\n- - - - - - - - - - - - - - - - - - - -"%self.source)
+		self.code=compile(self.source, "action: %s:%s"%(self.id, self.name), u"exec")
+
+	def on_execute(self, object, namespace):
+		app_module=managers.request_manager.get_request().application().id
+		__import__(app_module)
+		namespace={"request": scripting.request, "response": scripting.response, "application": scripting.application,
+			"server": scripting.server, "session": scripting.session, "self": object, "__package__": app_module}
+		exec self.code in namespace
+		
+class vscript_action(generic_action):
+
+	def __init(self, object, name, source):
+		action.__init__(self, object, name, source)
+		self.vsource=None
+
+	def compile(self):
+		app_module=managers.request_manager.get_request().application().id
+		generic_action.compile(self)
+		self.code, self.vsource=vcompile(self.source, package=app_module)
+
+	def on_execute(self, object, namespace):
+		vexecute(self.code, self.vsource, object, namespace)
