452187d83fed6f6bc10663c62ee0050714a8c1ca
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-06-20 19:17:16 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@6856 230e8929-42e3-3b44-a413-55659dd3a577

---
+++ src/scripting/wrappers/__init__.py
@@ -0,0 +1,17 @@
+
+from server import VDOM_server
+from application import VDOM_application
+from session import VDOM_session
+from request import VDOM_request
+from response import VDOM_response
+
+from obsolete_request import VDOM_obsolete_request
+
+
+server=VDOM_server()
+application=VDOM_application()
+session=VDOM_session()
+request=VDOM_request()
+response=VDOM_response()
+
+obsolete_request=VDOM_obsolete_request()
