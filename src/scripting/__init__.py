c486dda2f407d2566668cc873e425a3700983dd9
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-06-29 11:52:44 +0000

    Support for scripting log object
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@6936 230e8929-42e3-3b44-a413-55659dd3a577

--- src/scripting/__init__.py
+++ src/scripting/__init__.py
@@ -1,3 +1,3 @@
 
-from wrappers import server, application, session, request, response, obsolete_request
+from wrappers import server, application, session, log, request, response, obsolete_request
 from object import VDOM_object
--- src/scripting/wrappers/__init__.py
+++ src/scripting/wrappers/__init__.py
@@ -2,6 +2,7 @@
 from server import VDOM_server
 from application import VDOM_application
 from session import VDOM_session
+from log import VDOM_log
 from request import VDOM_request
 from response import VDOM_response
 
@@ -10,6 +11,7 @@ from obsolete_request import VDOM_obsolete_request
 
 server=VDOM_server()
 application=VDOM_application()
+log=VDOM_log()
 session=VDOM_session()
 request=VDOM_request()
 response=VDOM_response()
