65848dd9b0aa91145eaed6ede37435e2948d7df6
Michael Ovsyannikov <Michael Ovsyannikov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-07-18 10:40:34 +0000

    fix
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7001 230e8929-42e3-3b44-a413-55659dd3a577

--- src/source/cache.py
+++ src/source/cache.py
@@ -70,7 +70,7 @@ class VDOM_source_cache(object):
 		self.__cache_file_index = {}
 		self.__cache_list = []
 		self.__memused = 0
-		managers.source_swap.swap.clear()
+		managers.source_swap.clear()
 		
 	def clear_container_swap(self,application_id):
 		managers.file_manager.clear(file_access.cache, application_id, None)
