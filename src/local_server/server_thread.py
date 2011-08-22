811ccc9584d1c6297e4e90db01f8f437da574e3d
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-08-22 11:44:25 +0000

    Local server beta version
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7111 230e8929-42e3-3b44-a413-55659dd3a577

---
+++ src/local_server/server_thread.py
@@ -0,0 +1,47 @@
+
+import sys, os, thread, socket, select
+
+from version import VDOM_server_version
+from utils.threads import VDOM_thread
+import local_server
+
+
+class VDOM_local_server_thread(VDOM_thread):
+
+	def __init__(self):
+		VDOM_thread.__init__(self, name="Local Server")
+		port=VDOM_CONFIG["SERVER-LOCALHOST-PORT"]
+		self.__local_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.__local_socket.bind(('localhost', port))
+		local_server.send_to_card("online")
+		sys.stderr.write("Listening on localhost: %d\n"%port)
+
+	def handle_specific_request(self, command):
+		try:
+			local_server.execute(command)
+		except:
+			traceback.print_exc(file=debugfile)
+
+	def main(self):
+		sys.stderr.write("Start %s\n"%self.name)
+		while self.running:
+			ret = select.select([self.__local_socket], [], [], self.quantum)
+			for r in ret[0]:
+				if r == self.__local_socket:
+					(string, address) = self.__local_socket.recvfrom(1024)
+					if "stop" == string:
+						#self.__stop = True
+						debug("Stop from %s - ignore\n"%address[0])
+						#raise socket.error("")
+					elif "restart" == string:
+						#self.restart = True
+						#self.__stop = True
+						debug("Restart from %s - ignore\n"%address[0])
+						#raise socket.error("")
+					else:
+						# create thread to handle specific request from localhost
+						thread.start_new_thread(self.handle_specific_request, (string, ))
+
+	def stop(self):
+		sys.stderr.write("Stop %s\n"%self.name)
+		super(VDOM_local_server_thread, self).stop()
