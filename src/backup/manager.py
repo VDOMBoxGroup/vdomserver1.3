6c12bee2b0819c85fa8e31f1709fb9d95fbd4bd3
Sergey Baldin <Sergey Baldin@230e8929-42e3-3b44-a413-55659dd3a577>
2011-09-07 10:00:36 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7164 230e8929-42e3-3b44-a413-55659dd3a577

--- src/backup/manager.py
+++ src/backup/manager.py
@@ -1,15 +1,18 @@
-import backup, restore, storage_driver, scheduler, managers, version
+import managers, version
+from scheduler.task import VDOM_backup_task
+from storage_driver import backup_storage_manager
+from backup import backup
 
 
 class VDOM_backup_manager(object):
     
     def __init__(self):
-        self._backup = backup.VDOM_backup()
-        self._restore = restore.VDOM_restore()
-        self.backup_storage_manager = storage_driver.VDOM_backup_storage_manager()
-        self._scheduler = scheduler.VDOM_scheduler_manger()
-    def backup(self, app_id, path):
+        backup_storage_manager.restore()
+        
+    def backup(self, app_id, drv_id):
         appl = managers.xml_manager.get_application(app_id)
+        sd = backup_storage_manager.get_driver(drv_id)
+        path = sd.path
         meta_info = appl.info_map
         server_version = version.VDOM_server_version
         vh = managers.request_manager.current.server().virtual_hosting()
@@ -19,18 +22,54 @@ class VDOM_backup_manager(object):
             if vh.get_site(site) == app_id:
                 vhosts.append(site)
                 
-        self._backup.backup(app_id, path, meta_info, server_version, vhosts)
-    def restore(self, app_id, meta_info, server_version, vhost="default"):
-        pass
+        backup.backup(app_id, path, meta_info, server_version, vhosts)
+    
     def get_schedule(self, driver_id):
-        tasks = self._scheduler.get_tasks()
-        return tasks[driver_id]
+        schedule_list = managers.scheduler_manager.fetch(VDOM_backup_task)
+        schedule = []
+        for key in schedule_list:
+            if schedule_list[key][0].driver == driver_id:
+                schedule.append(schedule_list[key])
+        if schedule and len(schedule) == 1:
+            return schedule[0]
+        else:
+            return None
+        
     def add_schedule(self, driver_id, app_list, interval, rotation):
-        self._scheduler.get_task(driver_id, app_list, interval, rotation)
+        schedule = VDOM_backup_task(driver_id, app_list, rotation)
+        return managers.scheduler_manager.add_task(schedule, interval)
+    
+    def update_schedule(self, driver_id, app_list, interval, rotation):
+        schedule_list = managers.scheduler_manager.fetch(VDOM_backup_task)
+        for key in schedule_list:
+            if driver.id == schedule_list[key][0].driver:
+                schedule_list[key][0].applications = app_list
+                schedule_list[key][0].rotation = rotation
+                managers.scheduler_manager.update(key, schedule_list[key][0], interval)
+        
+    def del_schedule(self, task_id):
+        managers.scheduler_manager.dell_task(task_id)
+    
     def get_storages(self):
-        return self.backup_storage_manager.get_drivers()
+        return backup_storage_manager.get_drivers()
+    
     def add_storages(self, driver):
-        self.backup_storage_manager.add_driver(driver)
+        backup_storage_manager.add_driver(driver)
+        
+    def del_storage(self, driver):
+        backup_storage_manager.del_driver(driver)
+        schedule_list = managers.scheduler_manager.fetch(VDOM_backup_task)
+        remove_list = []
+        is_dirty_index = False
+        for key in schedule_list:
+            if driver.id == schedule_list[key][0].driver:
+                remove_list.append(key)
+                is_dirty_index = True
+        if is_dirty_index:
+            for key in remove_list:
+                managers.scheduler_manager.dell_task(key)
+                
+    
     def get_revision_list(self):
         pass
     
--- src/backup/storage_driver.py
+++ src/backup/storage_driver.py
@@ -16,8 +16,11 @@ class VDOM_storage_driver(object):
 class VDOM_backup_storage_manager(object):
     
     def __init__(self):
-        self.__index = managers.storage.read_object(VDOM_CONFIG["BACKUP-STORAGE-DRIVER-INDEX-RECORD"])
+        self.__index = {}
     
+    def restore(self):
+        self.__index = managers.storage.read_object(VDOM_CONFIG["BACKUP-STORAGE-DRIVER-INDEX-RECORD"])
+        
     def add_driver(self, driver):
         """add new driver"""
         if driver.id in self.__index:
@@ -33,7 +36,12 @@ class VDOM_backup_storage_manager(object):
         else:
             self.__index.pop(driver.id)
             managers.storage.write_object_async(VDOM_CONFIG["BACKUP-STORAGE-DRIVER-INDEX-RECORD"], self.__index)
-    
+            
     def get_drivers(self):
         return self.__index
     
+    def get_driver(self, id):
+        if id in self.__index:
+            return self.__index[id]
+
+backup_storage_manager = VDOM_backup_storage_manager()
