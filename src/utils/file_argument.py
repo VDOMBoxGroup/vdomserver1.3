cacab8530bff5f05ef0958750db743d10e554f10
Sergey Baldin <Sergey Baldin@230e8929-42e3-3b44-a413-55659dd3a577>
2011-09-09 09:15:35 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7190 230e8929-42e3-3b44-a413-55659dd3a577

--- src/utils/file_argument.py
+++ src/utils/file_argument.py
@@ -1,7 +1,10 @@
+import __builtin__
+
+
 class File_argument(object):
 	def __init__(self,fileobj, name):
-		self.__fileobj = fileobj
-		self.__name = name
+		self.fileobj = fileobj
+		self.name = name
 	def __getitem__(self, key):
 		if not isinstance(key, int):
 			raise TypeError
@@ -14,3 +17,16 @@ class File_argument(object):
 			return self.__name
 		else:
 			raise AttributeError
+		
+class Attachment(object):
+	def __init__(self, file_argument):
+		self.__filearg = file_argument
+	def __get_filename(self):
+		return self.__filearg.name
+	def __get_handler(self):
+		return self.__filearg.fileobj
+	
+	name = property(__get_filename)
+	handler = property(__get_handler)
+
+__builtin__.Attachment = Attachment
