1ae621b3f043dd16f7c30b9bbc7100a307adce32
Pavel Kim <Pavel Kim@230e8929-42e3-3b44-a413-55659dd3a577>
2011-09-14 11:28:35 +0000

    external drive driver for backup: mount and umount functions
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7210 230e8929-42e3-3b44-a413-55659dd3a577

--- src/backup/storage_driver.py
+++ src/backup/storage_driver.py
@@ -1,5 +1,7 @@
 import managers
 import utils.uuid
+from subprocess import *
+import shlex
 
 class VDOM_storage_driver(object):
     
@@ -11,8 +13,47 @@ class VDOM_storage_driver(object):
     
     def umount(self):
         pass
+
+class VDOM_sd_external_drive(VDOM_storage_driver):
+
+    def __init__(self):
+        self.id = str(utils.uuid.uuid4())
+        self.name = "External Drive for backup %s"%(self.id)
+        self.__path = None
+        self.ololo= "gogogo"
     
+    def mount(self):
+		# SEARCH for not mounted drives
+		cmd = """sh /opt/boot/mount_and_get_path.sh -s"""
+		out = Popen(shlex.split(cmd), stdin=PIPE, bufsize=-1, stdout=PIPE, stderr=PIPE, close_fds=True)
+		out.wait()
+		rc = out.returncode
+
+		if rc == 0:
+			drives = out.stdout.read()
+			cmd = """sh /opt/boot/mount_and_get_path.sh -d %s -m"""%drivers
+			out = Popen(shlex.split(cmd), stdin=PIPE, bufsize=-1, stdout=PIPE, stderr=PIPE, close_fds=True)
+			out.wait()
+			rc = out.returncode
+			if rc == 0:
+				self.__path = out.stdout.read()
+				return self.__path
+			else:
+				return False
+		elif rc == 1:
+			return False
+		
     
+    def umount(self):
+		cmd = """sh /opt/boot/mount_and_get_path.sh -d %s -u"""%self.__path
+		out = Popen(shlex.split(cmd), stdin=PIPE, bufsize=-1, stdout=PIPE, stderr=PIPE, close_fds=True)
+		out.wait()
+		rc = out.returncode
+		if rc == 0:
+			return True
+		else:
+			return False
+
 class VDOM_backup_storage_manager(object):
     
     def __init__(self):
