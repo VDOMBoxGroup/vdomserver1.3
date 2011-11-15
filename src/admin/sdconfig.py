73f62ecee17c7ab53c6d64f492efd321489f2ec3
Sergey Baldin <Sergey Baldin@230e8929-42e3-3b44-a413-55659dd3a577>
2011-11-15 10:00:34 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7543 230e8929-42e3-3b44-a413-55659dd3a577

--- src/admin/sdconfig.py
+++ src/admin/sdconfig.py
@@ -1,8 +1,9 @@
-import os, traceback, json
+import os, traceback, json, re
 import managers
 from utils.system import get_external_drives
 from utils.exception import VDOM_exception
 from backup.storage_driver import VDOM_sd_external_drive
+
 def run(request):
         sess = request.session()
         if not sess.value("appmngmtok"):
@@ -12,53 +13,326 @@ def run(request):
         args = request.arguments().arguments()
         applst = managers.xml_manager.get_applications()
         dev_list = VDOM_sd_external_drive.get_device_list()
-        if "devid" in args and args["devid"][0] != "":
-                sd = managers.backup_manager.get_storage(args["devid"][0])
-                schedule = managers.backup_manager.get_schedule(args["devid"][0])[1]
-                inter = schedule[1] if schedule else ("00", "00", "30", "*", "*")
-            	applist = schedule[0].applications if schedule else applst
-	if "devselect" in args or "driver" in args:
-		if "devselect" in args and args["devselect"][0] != "":
-			if "addsd" in args: 
-				driver = VDOM_sd_external_drive(args["devselect"][0])
-		if "driver" in args:
-			if "update" in args:
-				driver = managers.backup_manager.get_storage(args["driver"][0])
-			elif "delete" in args:
-				driver = managers.backup_manager.get_storage(args["driver"][0])
-				managers.backup_manager.del_storage(driver)
-				request.redirect("/appbackup.py")
-				return
-                path = driver.mount()
-                if not path:
-                        request.write("Mount failed")
-                        raise VDOM_exception("Mount failed")
-                else:
-			if "hour" in args and "minutes" in args and "days" in args and "appl[]" in args:
-                    		managers.backup_manager.add_storage(driver)
-                    		interval = ('%s' % args["minutes"][0],
-				            '%s' % args["hour"][0],
-				            '*/%s' % args["days"][0], '*', '*')
-                                if "addsd" in args:
-                                        managers.backup_manager.add_schedule(driver.id, args["appl[]"], interval, rotation="0")
-                                elif "update" in args:
-                                        managers.backup_manager.update_schedule(driver.id, args["appl[]"], interval, rotation="0")
-                                driver.umount()
-                        request.redirect("/appbackup.py")
-			return
+	dev_option_tag = apps_tag = ""
+	driver = None
+	
+	if "erase" in args and args["erase"][0] != "":
+		driver = managers.backup_manager.get_storage(args["erase"][0])
+		managers.backup_manager.del_storage(driver)
+		driver = None
+	if "devid" in args:
+		driver = managers.backup_manager.get_storage(args["devid"][0])
+		hidden_tag = """
+<input type="hidden" name="devid" value="%s">""" % driver.id
+	else:
+		hidden_tag = ""
+		apps_tag = ""
+		for app in applst:
+			appname = managers.xml_manager.get_application(app).name
+			apps_tag += """
+         <p><input type="checkbox" name="backup_app[]" value="%(appid)s" checked>%(appname)s <span>GUID %(appid)s</span></input></p>""" % \
+			         {"appid": app, "appname": appname}
+		
+		hourly_selected = ""
+		week_display = ''
+		daily_selected = " selected"
+		week_days = "*"
+		daily_display = ''
+		hourly_display = ' style="display:none;"'
+		backup_time = "00-00"
+	
+	if "save" in args:
+		ok = True
+		if not driver:
+			driver = VDOM_sd_external_drive(args["devselect"][0])
+		if "backup_app[]" in args:
+			backup_apps = args["backup_app[]"]
+			if "week-day[]" in args and "daily_int" in args and args["int1"][0] == "daily" and re.match("^([01]?[0-9]|2[0-3])-[0-5][0-9]", args["daily_int"][0]):
+				days_of_week = "*" if len(args["week-day[]"]) == 7 else ",".join(args["week-day[]"])
+				minutes = args["daily_int"][0].split('-')[1]
+				hours = args["daily_int"][0].split('-')[0]
+				interval = (minutes, hours, "*", "*", days_of_week)
+			elif args["int1"][0] == "hourly" and "hourly_int" in args and re.match("^([1-9]|1[0-9]|2[0-4])", args["hourly_int"][0]):
+				hours = "*/" + args["hourly_int"][0]
+				interval = ("*", hours, "*", "*", "*")
+			elif args["int1"][0] == "daily" and "week-day[]" not in args:
+				request.write('<script language="javascript">parent.server.document.getElementById("MsgSvrInfo").innerHTML="Error: you must choose one or more days!";</script>')
+				ok = False
+			else:
+				request.write('<script language="javascript">parent.server.document.getElementById("MsgSvrInfo").innerHTML="Error: Incorrect time format!";</script>')
+				ok = False
+			if ok:
+				if driver.change_device(args["devselect"][0]):
+					managers.backup_manager.add_storage(driver)
+				if not managers.backup_manager.update_schedule(driver.id, backup_apps, interval, "0"):
+					managers.backup_manager.add_storage(driver)
+					managers.backup_manager.add_schedule(driver.id, backup_apps, interval, "0")
+		else:
+			request.write('<script language="javascript">parent.server.document.getElementById("MsgSvrInfo").innerHTML="Error: There is no application for backup";</script>')
+	for dev in dev_list:
+		dev_option_tag += "<option value='%(dev)s'%(selected)s>%(dev)s</option>" % {"dev": dev, "selected": " selected" if driver and dev == driver.dev else ""}		
+	if "devid" in args or "save" in args:
+		schedule = managers.backup_manager.get_schedule(driver.id)
+		if schedule:			
+			backup_apps = schedule[1][0].applications
+			apps_tag = ""
+			for app in applst:
+				appname = managers.xml_manager.get_application(app).name
+				apps_tag += """
+			 <p><input type="checkbox" name="backup_app[]" value="%(appid)s"%(checked)s>%(appname)s <span>GUID %(appid)s</span></input></p>""" % \
+					 {"appid": app, "appname": appname, "checked": " checked" if app in backup_apps else ""}
+			schedule1 = schedule[1][1]
+			if "*" in schedule1[1]:
+				hourly_selected = " selected"
+				week_display = ' style="display:none;"'
+				daily_selected = ""
+				week_days = "*"
+				daily_display = ' style="display:none;"'
+				hourly_display = ''
+				backup_time = "1" if schedule1[1] == "*" else schedule1[1].split('/')[1]
+			else:
+				daily_selected = " selected"
+				week_display = ""
+				hourly_selected = ""
+				week_days = schedule1[4]
+				daily_display = ''
+				hourly_display = ' style="display:none;"'
+				backup_time = schedule1[1] + "-" + schedule1[0]
+		
+
         request.write("""<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Uninstall</title>
 <style type="text/css">
+body {
+margin:0;
+padding:0;
+font-family:Arial;
+background-color:#f8f8f8;
+}
 
 .Texte {
-	font-family: Tahoma, Verdana, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	color: #000000;
+ font-family: Tahoma, Verdana, Arial, Helvetica, sans-serif;
+ font-size: 12px;
+ color: #000000;
 }
-select.dev{
-        width: 123px;
+
+.wrapper {
+
+width:100%;
+height:100%;
+margin:0px;
+padding:0;
+}
+
+.block {
+width:605px;
+background-color:#fff;
+border:1px solid #ccc;
+margin:15px;
+ box-shadow: 0 0 10px #c6c6c6;
+    -moz-box-shadow: 0 0 10px #c6c6c6;
+    -webkit-box-shadow: 0 0 10px #c6c6c6;
+}
+
+.block h2 {
+color:#5e5e5e;
+font-size:18px;
+font-weight:normal;
+margin-left:18px;
+}
+
+table.devices {
+width:100%;
+font-size:14px;
+border-collapse:collapse;
+}
+table.devices td {
+border-bottom:1px solid #d6d6d6;
+line-height:28px;
+
+}
+
+table.devices td.name {
+padding-left:24px;
+}
+
+table.devices td.ext-drive {
+background: url(images/exthdd.png);
+background-position:left 50%;
+background-repeat:no-repeat;
+}
+table.devices td.cloud-drive {
+background: url(images/cloud.png);
+background-position:left 50%;
+background-repeat:no-repeat;
+}
+
+table.devices td.check {
+width:24px;
+padding-left:15px;
+}
+
+table.devices td.options a {
+font-size:10px;
+color:#7f7f7f;
+padding-right:10px;
+text-decoration:none;
+} 
+table.devices td.options a:hover {
+color:#000;
+}
+table.devices td.options {
+text-align:right;
+} 
+
+.submit-gray {
+background:#e9e9e9;
+width:100%;
+height:50px;
+text-align:center;
+}
+.submit-gray input {
+margin-top:15px;
+}
+
+.block select{
+width:240px;
+margin-left:15px;
+}
+.radiob, .checkbox {
+margin-left:15px;
+margin-top:15px;
+}
+
+.radiob p{
+font-size:14px;
+
+}
+.checkbox p{
+font-size:12px;
+font-weight:bold;
+line-height:16px;
+}
+.radiob p span,.checkbox p span{
+color:#4a4a4a;
+font-size:10px;
+font-weight:normal;
+}
+.fields {
+width:240px;
+
+}
+
+.fields div{
+clear:both;
+margin-left:20px;
+margin-top:15px;
+}
+
+.fields select, .fields input{
+float:right;
+display:block;
+position:relative;
+margin-bottom:10px;
+width:150px;
+}
+
+.fields label {
+float:left;
+line-height:24px;
+font-size:12px;
+display:block;
+position:relative;
+}
+#x_hourly input{
+float: left;
+width: 30px;
+margin: 0 10;
+}
+.clear {
+clear:both;
+width:100%;
+display:block;
+}
+.week {
+clear:none !important;
+position:absolute;
+top:104px;
+left:270px;
+}
+.week .week-day {
+float:left;
+padding:3px;
+font-size:10px;
+text-align:center;
+}
+.week label,.week input[type=checkbox] {
+display:block;
+margin:0;
+padding:0;
+}
+.hdd-state {
+width:150px;
+height:128px;
+border:1px solid #dcdcdc;
+background: url(images/gr-bg.png);
+background-position:bottom left;
+background-repeat:repeat-x;
+padding:8px;
+margin:10px;
+}
+.hdd-state h2 {
+color:#000;
+margin:0;
+padding:0;
+font-size:14px;
+}
+
+.progress {
+width:120px;
+height:14px;
+border:1px solid #989898;
+ border-radius: 10px;
+    -moz-border-radius: 10px;
+    -webkit-border-radius: 10px;
+	padding:1px;
+	margin-top:15px;
+}
+.progress .bar {
+width:120px;
+height:14px;
+background:#000;
+ border-radius: 10px;
+    -moz-border-radius: 10px;
+    -webkit-border-radius: 10px;
+}
+.progress .bar-holder {
+overflow:hidden;
+width:50%;
+
+}
+.free-mem {
+font-size:12px;
+margin-top:15px;
+}
+
+.hdd-state .erase {
+margin-top:15px;
+}
+
+.hdd-state .erase a{
+font-size:14px;
+color:#fe0000;
+}
+
+.fl-left {
+float:left;
+}
+
+.fl-right {
+float:right;
 }
 </style>
 
@@ -66,6 +340,17 @@ select.dev{
 function LoadImgWait(){
 	document.getElementById('Imgload').style.display='';
 	parent.server.document.getElementById("MsgSvrInfo").innerHTML="Saving backup configuration...";
+}
+function ChangeBackup(obj){
+	if (obj.value == "hourly"){
+	  document.getElementsByClassName('week')[0].style.display='none';
+	  document.getElementById('x_daily').style.display='none';
+	  document.getElementById('x_hourly').style.display='';
+	}else if (obj.value == "daily"){
+	  document.getElementsByClassName('week')[0].style.display='';
+	  document.getElementById('x_daily').style.display='';
+	  document.getElementById('x_hourly').style.display='none';
+	}
 }
 </script>
 </head>
@@ -75,126 +360,69 @@ function LoadImgWait(){
 
 <div id="Imgload" style="position:absolute; z-index:100; top:0px; left:0px; width:100%; height:100%; background-color:#FFFFFF; display:none" align="center"><br><br><img src="/images/loading.gif" border="0"></div>
 
-<p class="Texte"><a href="menuappli.py">Application Management</a> &gt; Backup</p>
-<table width="100%" height="85%" border="0">
-   <tr>
-     <td>
-""")
-        request.write("""
-	<form name="configBackup" method=post onsubmit="LoadImgWait();" action="/sdconfig.py" enctype="multipart/form-data">""")
-	if "devid" in args:
-		request.write("""
-	<input type="hidden" name="driver" value="%s">""" % sd.id)
+<p class="Texte"><a href="menuappli.py">Application Management</a> &gt; <a href="appbackup.py">Backup</a> &gt; Config</p>""")
 	request.write("""
-	<table>
-	  <tr>
-	    <td>
-	     <p><span>Select device: </span>
-	      <select class="dev" name="devselect">""")
-        for devs in dev_list:
-                request.write("""
-		  <option value="%(dev)s">%(dev)s</option>""" % {"dev": devs})
-        request.write("""
-	      </select>
-	     </p> 
-	    </td>
-	  </tr>
-	  <tr>
-	    <td>
-	      <p><span>Backup at: </span>
-	        <input type="text" size="2" name="hour" onchange="hour_test(this);"><span> : </span>
-	        <input type="text" size="2" name="minutes" onchange="minute_test(this);">
-	      </p>
-	    </td>
-	  </tr>
-	  <tr>
-	    <td>
-	      <p><span>Backup every: </span>
-	        <label><input type="text" size="2" name="days"> day(s)</label>
-	      </p>
-	    </td>
-	  </tr>
-	  <tr>
-	    <td>
-	      <p><span>Rotate: </span>
-	        <input type="text" size="3" name="rotate" onchange="rot_test(this);">
-	      </p>
-	    </td>
-	  </tr>  
-	  <tr>
-	    <td>""")
-        for appid in applst:
-                app = managers.xml_manager.get_application(appid)
-                appname = app.name
-                request.write("""
-	     <p><input type="checkbox" name="appl[]" value="%s"><span> Name: "%s" GUID: (%s)</span></p>""" % (appid, appname, appid))
-        request.write("""
-	    </td>
-	  </tr>
-          <tr>
-            <td>
-              <input type="submit" name="addsd" value="Add">
-              <input type="submit" name="update" value="Update">
-              <input type="submit" name="delete" value="Delete">
-            </td>
-          </tr>  
-	</table>
-	</form>
-     </td>
-   </tr>
- </table>""")
-        if "devid" in args and args["devid"][0] != "":
-                request.write("""
-<script language="javascript">
-   function in_array(needle, haystack, strict) {
-     var found = false, key, strict = !!strict;
-     for (key in haystack) {
-       if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {
-         found = true;
-         break;
-       }
-     }
-     return found;
-   }
-   
-   var form = document.configBackup;
-   form["addsd"].disabled=true;
-   var el = form["devselect"];
-   el.disabled=true;
-   var applst = %s;
-   for (var i=0; i<el.options.length; i++){
-      if (el.options[i].value == '%s'){
-        el.options[i].selected=true;
-      }else{
-        el.options[i].selected=false;
-      }
-   }
-   form["hour"].value = '%s';
-   form["minutes"].value = '%s';
-   form["days"].value = '%s';
-   var check = document.getElementsByName("appl[]")
-   for (var i=0; i<check.length; i++){
-      if (in_array(check[i].value, applst)) check[i].checked = true; else check[i].checked = false;
-   }
+<form name="configBackup" method=post enctype="multipart/form-data">%s
+<div class="wrapper"> 
+ <div class="block">
+  <h2><img src="images/exthdd.png" align="absmiddle"/> Config External Drive 1</h2>
+  <div class="fields fl-left">""" % hidden_tag)
+	request.write("""
+   <div><label for="devselect">Device :</label><select name="devselect">%s</select></div>""" % dev_option_tag)
+	request.write("""
+   <div><label for="int1">Backup :</label><select name="int1" onchange="ChangeBackup(this);"><option value="daily"%s>Daily</option><option value="hourly"%s>Hourly</option></select></div>""" % (daily_selected, hourly_selected))
+	request.write("""
+   <div id="x_daily"%(display)s><label for="daily_int">Backup at : </label><input type="text" name="daily_int" value="%(value)s"></div>""" % {"display": daily_display, "value": backup_time})
+	request.write("""
+   <div id="x_hourly"%(display)s><label for="hourly_int">Backup every </label><input type="text" name="hourly_int" value="%(value)s"><label for="hourly_int"> hour(s)</label></div>""" % {"display": hourly_display, "value": backup_time})
+	request.write("""
+   <div class="clear"> </div>
+  </div>""")
+	request.write("""
+  <div class="week"%(display)s>
+	<div class="week-day"><label for="mon">M</label><input id="mon" name="week-day[]" value="1" type="checkbox"%(mon)s/></div>
+	<div class="week-day"><label for="tue">T</label><input id="tue" name="week-day[]" value="2" type="checkbox"%(tue)s/></div>
+	<div class="week-day"><label for="wed">W</label><input id="wed" name="week-day[]" value="3" type="checkbox"%(wed)s/></div>
+	<div class="week-day"><label for="thu">T</label><input id="thu" name="week-day[]" value="4" type="checkbox"%(thu)s/></div>
 
-</script>""" % (json.dumps(applist), sd.dev, inter[1], inter[0], inter[2].strip('*/') if inter[2] else "30"))
-        if not "devid" in args:
-                request.write("""
-<script language="javascript">var form = document.configBackup; form["update"].disabled=true; form["delete"].disabled=true;</script>""")
-        request.write("""
-<script language="javascript">
-function hour_test(input) {
-    var re = /^(([0,1][0-9])|(2[0-3]))$/;
-    if (!(re.test(input.value))) input.value = '';
-}
-function minute_test(input) {
-    var re = /^[0-5][0-9]$/;
-    if (!(re.test(input.value))) input.value = '';
-}
-function rot_test(input){
-    var re = /\d*/;
-    if (!(re.test(input.value))) input.value = '';
-}
-</script>
+	<div class="week-day"><label for="fri">F</label><input id="fri" name="week-day[]" value="5" type="checkbox"%(fri)s/></div>
+	<div class="week-day"><label for="sat">S</label><input id="sat" name="week-day[]" value="6" type="checkbox"%(sat)s/></div>
+	<div class="week-day"><label for="sun">S</label><input id="sun" name="week-day[]" value="7" type="checkbox"%(sun)s/></div>
+	<div class="clear"> </div>
+  </div>""" % {"display": week_display,
+	       "mon": " checked" if "1" in week_days or week_days == '*' else "",
+	       "tue": " checked" if "2" in week_days or week_days == '*' else "",
+	       "wed": " checked" if "3" in week_days or week_days == '*' else "",
+	       "thu": " checked" if "4" in week_days or week_days == '*' else "",
+	       "fri": " checked" if "5" in week_days or week_days == '*' else "",
+	       "sat": " checked" if "6" in week_days or week_days == '*' else "",
+	       "sun": " checked" if "7" in week_days or week_days == '*' else ""})
+	request.write("""
+  <div class="hdd-state fl-right" align="center">
+	<div class="head"><h2><img src="images/exthdd.png" align="absmiddle"/> Backup HDD 1</h2></div>
+
+	<div class="progress" align="left">
+		<div class="bar-holder">
+		  <div class="bar"></div>
+		</div>
+	</div>
+
+	<div class="free-mem">35Gb of 50Gb is free</div>
+
+	<div class="erase"><a href="sdconfig.py?erase=%(dev)s">Erase this storage</a></div>
+
+  </div>
+  <div class="clear"> </div>
+ </div>""" % {"dev": driver.id if driver else ""})
+	request.write("""
+ <div class="block">
+	<h2>Backup following applications :</h2>
+	<div class="checkbox">%s
+        </div>
+ </div>""" % apps_tag)
+	request.write("""
+<center><input type="submit" name="cancel" value="Cancel"/> &nbsp &nbsp <input type="submit" name="save" value="Save changes"/></center>
+</div>
+</form>
 </body>
 </html>""")
