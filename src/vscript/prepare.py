d9d3da35300adffa7a76fbb62ffa505bff24e620
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-11-23 12:41:55 +0000

    Preliminary update: ply & vscript
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7572 230e8929-42e3-3b44-a413-55659dd3a577

--- src/vscript/prepare.py
+++ src/vscript/prepare.py
@@ -6,15 +6,19 @@ import ply.yacc as yacc
 
 import lexemes, syntax
 
+
 tablepath=os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), VDOM_CONFIG["TEMP-DIRECTORY"]))
-tablename="vscript_%s_table"%os.path.splitext(os.path.basename(sys.argv[0]))[0]
+lexer_table_name="vscript_%s_lexer_table"%os.path.splitext(os.path.basename(sys.argv[0]))[0]
+parser_table_name="vscript_%s_parser_table"%os.path.splitext(os.path.basename(sys.argv[0]))[0]
+optimize=1
+
 
 if VDOM_CONFIG["DISABLE-VSCRIPT"]:
 	print "VScript are disabled through configuration"
 	lexer=None
 	parser=None
 else:
-	print "Preparing VScript engine... ",
-	lexer=lex.lex(module=lexemes, debug=0)
-	parser=yacc.yacc(module=syntax, debug=0, start="source", tabmodule=tablename, outputdir=tablepath)
+	print "Preparing VScript engine...",
+	lexer=lex.lex(module=lexemes, debug=0, optimize=optimize, outputdir=tablepath, lextab=lexer_table_name)
+	parser=yacc.yacc(module=syntax, debug=0, start="source", optimize=optimize, outputdir=tablepath, tabmodule=parser_table_name)
 	print "Done"
