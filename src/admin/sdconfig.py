1629d9400c322279e2cb734bb3f640d819746ccb
Sergey Baldin <Sergey Baldin@230e8929-42e3-3b44-a413-55659dd3a577>
2011-12-06 06:05:05 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7640 230e8929-42e3-3b44-a413-55659dd3a577

--- src/admin/sdconfig.py
+++ src/admin/sdconfig.py
@@ -89,7 +89,7 @@ def run(request):
 		else:
 			request.write('<script language="javascript">parent.server.document.getElementById("MsgSvrInfo").innerHTML="Error: There is no application for backup";</script>')
 	for dev in dev_list:
-		dev_option_tag += "<option value='%(dev)s'%(selected)s>%(devname)s</option>" % {"dev": dev[0], "devname": dev[1], "selected": " selected" if driver and dev[0] == driver.dev else ""}		
+		dev_option_tag += "<option value='%(dev)s'%(selected)s>%(devname)s</option>" % {"dev": dev[0], "devname": dev[1], "selected": " selected" if driver and hasattr(driver, 'dev') and dev[0] == driver.dev else ""}		
 	if "devid" in args or "save" in args:
 		schedule = managers.backup_manager.get_schedule(driver.id)
 		if schedule:			
