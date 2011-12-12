b79d38c5f03a2364a391e13a3b554831e3723f9a
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2011-12-12 17:36:52 +0000

    VScript: WHOLE remote calls support
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7691 230e8929-42e3-3b44-a413-55659dd3a577

--- src/vscript/wrappers/__init__.py
+++ src/vscript/wrappers/__init__.py
@@ -8,3 +8,7 @@ from .sqlite import v_vdomdbconnection, v_vdomdbrecordset
 from .imaging import v_vdomimaging
 from .vdombox import v_vdombox
 from .remote import v_remoteserver
+from .whole import v_wholeconnection, v_wholeapplication, \
+	v_wholeerror, v_wholeconnectionerror, v_wholenoconnectionerror, \
+	v_wholeremotecallerror, v_wholeincorrectresponse, \
+	v_wholenoapierror, v_wholenoapplication
