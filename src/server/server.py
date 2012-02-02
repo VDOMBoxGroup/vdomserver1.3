6061e68fe83fd806ce9048e1d2638052728f5540
Sergey Baldin <Sergey Baldin@230e8929-42e3-3b44-a413-55659dd3a577>
2012-02-02 10:46:56 +0000

    webdav
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7890 230e8929-42e3-3b44-a413-55659dd3a577

--- src/server/server.py
+++ src/server/server.py
@@ -5,6 +5,7 @@ from utils.threads import VDOM_server as VDOM_threads_server
 #from memory import VDOM_xml_synchronizer
 from local_server import VDOM_local_server_thread
 from web import VDOM_web_server_thread
+from webdav_server import VDOM_webdav_server_thread
 
 
 class VDOM_server(VDOM_threads_server):
@@ -22,3 +23,6 @@ class VDOM_server(VDOM_threads_server):
 		
 		self.__web_server_thread=VDOM_web_server_thread()
 		self.__web_server_thread.start()
+		
+		self.__webdav_server_thread=VDOM_webdav_server_thread()
+		self.__webdav_server_thread.start()
