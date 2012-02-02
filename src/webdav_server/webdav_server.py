0014e7316e07c0bdebcac2bdbab1be8146b7e460
Sergey Baldin <Sergey Baldin@230e8929-42e3-3b44-a413-55659dd3a577>
2012-02-02 05:45:53 +0000

    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7882 230e8929-42e3-3b44-a413-55659dd3a577

--- src/webdav_server/webdav_server.py
+++
@@ -1,5 +0,0 @@
-from BaseHTTPServer import HTTPServer
-from SocketServer import ThreadingMixIn
-
-class VDOM_webdav_server(ThreadingMixIn, HTTPServer):
-    """Handle requests in a separate thread."""
