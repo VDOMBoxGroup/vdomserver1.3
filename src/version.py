49f2bedb0156a54d1ae324f3783a887cf7f23246
Michael Ovsyannikov <Michael Ovsyannikov@230e8929-42e3-3b44-a413-55659dd3a577>
2012-02-13 12:55:26 +0000

    VDOM current is now in 1.3 version
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@7915 230e8929-42e3-3b44-a413-55659dd3a577

--- src/version.py
+++ src/version.py
@@ -1,4 +1,4 @@
 
-svn_version = "7844"
-VDOM_server_version = "1.2.%s" % svn_version
+svn_version = "7915"
+VDOM_server_version = "1.3.%s" % svn_version
 VDOM_server_name = "VDOMv2 application server/" + VDOM_server_version
