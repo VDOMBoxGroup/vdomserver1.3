86aab3316860f3711bdaaa303488b5d9d838cb4e
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2012-05-03 22:55:35 +0000

    Watcher
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@8132 230e8929-42e3-3b44-a413-55659dd3a577

--- src/watcher/watcher.py
+++ src/watcher/watcher.py
@@ -1,14 +1,12 @@
 
-import sys, traceback, os, socket, select, gc, re, resource
+import sys, traceback, os, socket, select, gc, re, resource, collections
 from utils.threads import VDOM_thread
 from utils.tracing import get_thread_trace, get_threads_trace
 
 
 class VDOM_watcher(VDOM_thread):
 
-	main_pattern=re.compile("""\s*<(?P<name>[A-Za-z][0-9A-Za-z]*)""")
-	ping_pattern=re.compile("""\s*<ping(?:\s*/>|\s*>\s*</ping>\s*)$""")
-	state_pattern=re.compile("""\s*<state(?:\s+thread="(?P<thread>\d+)")?(?:\s*/>|\s*>\s*</state>\s*)$""")
+	pattern=re.compile("""\s*<action\s+name=(?P<quote1>['"])(?P<name>[A-Za-z][0-9A-Za-z]*)(?P=quote1)""")
 
 	def __init__(self):
 		VDOM_thread.__init__(self, name="Watcher")
@@ -16,9 +14,8 @@ class VDOM_watcher(VDOM_thread):
 		self._port=VDOM_CONFIG["WATCHER-PORT"]
 		self._socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self._socket.bind((self._address, self._port))
-		self._allowable={
-			"ping": (self.ping, self.ping_pattern),
-			"state": (self.state, self.state_pattern)}
+		self._routines={routine.__name__: (routine, re.compile("\s*%s\s*$"%routine.__doc__)) \
+			for routine in (self.ping, self.state, self.analyse)}
 
 	def main(self):
 		while self.running:
@@ -26,9 +23,9 @@ class VDOM_watcher(VDOM_thread):
 			if reading:
 				try:
 					message, address=self._socket.recvfrom(512)
-					match=self.main_pattern.match(message)
+					match=self.pattern.match(message)
 					try:
-						routine, pattern=self._allowable[match.group("name")]
+						routine, pattern=self._routines[match.group("name")]
 						match=pattern.match(message)
 						if not match: raise ValueError
 						try:
@@ -47,61 +44,87 @@ class VDOM_watcher(VDOM_thread):
 						pass
 
 	def ping(self, match):
-		return "<ping/>"
+		"""<action\s+name=(?P<quote1>['"])ping(?P=quote1)\s*(?:/>|>\s*</action>)"""
+		return "<reply/>"
 
 	def state(self, match):
+		"""<action\s+name=(?P<quote1>['"])state(?P=quote1)\s*""" \
+		"""(?:/>|>\s*(?:<option\s+name=(?P<quote2>['"])thread(?P=quote2)\s*>\s*(?P<thread>\d+)\s*</option>\s*)?</action>)"""
 		if match.group("thread"):
-			data=get_thread_trace(int(match.group("thread")))
-			if not data:
+			try:
+				thread, smart, stack=get_thread_trace(int(match.group("thread")))
+			except:
 				return "<error>no thread</error>"
-			thread, smart, stack=data
-			frames="".join((
-				"<frame>" \
-					"<path>%s</path>" \
-					"<line>%s</line>" \
-					"<function>%s</function>" \
-				"</frame>"%(path.encode("xml"), line, function.encode("xml")) for path, line, function, statement in stack))
 			return \
-				"<state>" \
-					"<thread id=\"%s\"%s%s>" \
-						"<name>%s</name>" \
+				"<reply>" \
+					"<thread name=\"%s\" id=\"%d\" daemon=\"%s\" smart=\"%s\">" \
 						"<stack>%s</stack>" \
 					"</thread>" \
-				"</state>"%(thread.ident,
-					" daemon=\"yes\"" if thread.daemon else "",
-					" smart=\"yes\"" if smart else "",
-					thread.name.encode("xml"), frames)
+				"</reply>"% \
+					(thread.name.encode("xml"), thread.ident,
+					"yes" if thread.daemon else "no", "yes" if smart else "no", "".join((
+					"<frame name=\"%s\" path=\"%s\" line=\"%d\"/>"% \
+						(name.encode("xml"), path.encode("xml"), line) for path, line, name, statement in stack)))
 		else:
 			try:
 				usage=resource.getrusage(resource.RUSAGE_SELF)
 				process= \
 					"<process id=\"%d\">" \
-						"<usage>" \
-							"<utime>%.3f</utime>" \
-							"<stime>%.3f</stime>" \
-							"<maxrss>%d</maxrss>" \
-							"<ixrss>%d</ixrss>" \
-							"<idrss>%d</idrss>" \
-						"</usage>" \
-					"</process>"%(os.getpid(), usage.ru_utime, usage.ru_stime, usage.ru_maxrss, usage.ru_ixrss, usage.ru_idrss)
+						"<usage utime=\"%.3f\" stime=\"%.3f\" maxrss=\"%d\" idrss=\"%d\" ixrss=\"%d\"/>" \
+					"</process>"% \
+						(os.getpid(), usage.ru_utime, usage.ru_stime, usage.ru_maxrss, usage.ru_idrss, usage.ru_ixrss)
 			except:
 				process="<process id=\"%d\"/>"%os.getpid()
-			threads="".join((
-				"<thread id=\"%s\">%s</thread>"%(thread.ident, thread.name.encode("xml")) for thread, smart, stack in get_threads_trace()))
-			counter0, counter1, counter2=gc.get_count()
+			count0, count1, count2=gc.get_count()
 			return \
-				"<state>" \
+				"<reply>" \
 					"%s" \
 					"<threads>%s</threads>" \
-					"<garbagecollector%s>" \
-						"<counters>" \
-							"<counter generation=\"0\">%d</counter>" \
-							"<counter generation=\"1\">%d</counter>" \
-							"<counter generation=\"2\">%d</counter>" \
-						"</counters>" \
-						"<garbage number=\"%d\"/>" \
-						"<objects number=\"%d\"/>" \
+					"<garbagecollector>" \
+						"<objects>%d</objects>" \
+						"<garbage>%d</garbage>" \
+						"<collection>"\
+							"<generation>%d</generation>" \
+							"<generation>%d</generation>" \
+							"<generation>%d</generation>" \
+						"</collection>"\
 					"</garbagecollector>" \
-				"</state>"%(process, threads,
-					"" if gc.isenabled() else " state=\"disabled\"",
-					counter0, counter1, counter2, len(gc.garbage), len(gc.get_objects()))
+				"</reply>"% \
+					(process, "".join(("<thread id=\"%s\" name=\"%s\"%s/>"% \
+							(thread.ident, thread.name.encode("xml"), "" if thread.is_alive() else " alive=\"no\"") \
+								for thread, smart, stack in get_threads_trace())),
+						len(gc.get_objects()), len(gc.garbage), count0, count1, count2)
+
+	def analyse(self, match):
+		"""<action\s+name=(?P<quote1>['"])analyse(?P=quote1)\s*""" \
+		""">\s*<option\s+name=(?P<quote2>['"])(?P<option>objects|garbage)(?P=quote2)\s*(?:/>|>\s*</option>)\s*</action>"""
+		if match.group("option"):
+			option=match.group("option")
+			if option=="objects":
+				reference=collections.defaultdict(int)
+				for item in gc.get_objects():
+					reference[type(item)]+=1
+				counters="".join((
+					"<counter object=\"%s\">%d</counter>"% \
+						(item.__name__ if item.__module__=="__builtin__" else "%s.%s"%(item.__module__, item.__name__), counter) \
+							for item, counter in reference.iteritems()))
+				return \
+					"<reply>" \
+						"<counters>" \
+							"%s" \
+						"</counters>" \
+					"</reply>"%(counters)
+			elif option=="garbage":
+				reference=collections.defaultdict(int)
+				for item in gc.garbage:
+					reference[type(item)]+=1
+				counters="".join((
+					"<counters object=\"%s\">%d</counters>"% \
+						(item.__name__ if item.__module__=="__builtin__" else "%s.%s"%(item.__module__, item.__name__), counter) \
+							for item, counter in reference.iteritems()))
+				return \
+					"<reply>" \
+						"<counters>" \
+							"%s" \
+						"</counters>" \
+					"</reply>"%(counters)
