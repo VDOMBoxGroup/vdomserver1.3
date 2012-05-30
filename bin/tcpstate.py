f9ae7ba57c28db90b5f95430a750c157fc99526f
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2012-05-30 22:22:50 +0000

    Watcher: Utilities
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@8212 230e8929-42e3-3b44-a413-55659dd3a577

---
+++ bin/tcpstate.py
@@ -0,0 +1,75 @@
+
+import sys, socket, select, time, re
+
+
+address="127.0.0.1"
+port=10101
+frame=64*1024
+timeout=0.5
+request="<action name=\"state\"/>"
+pattern=re.compile("\s*(?:<reply>.+</reply>|</reply>)\s*", re.DOTALL)
+handler=lambda message: None
+
+
+def decode(input, errors='strict'):
+	decode_regex=re.compile("(?:&#(\d{1,5});)|(?:&#x(\d{1,5});)|(&\w{1,8};)")
+	decode_table={"&lt;": "<", "&gt;": ">", "&quot;": "\"", "&apos;": "'", "&amp;": "&"}
+	def substitute(match):
+		code, xcode, entity=match.group(1, 2, 3)
+		return unichr(int(code)) if code else unichr(int(xcode, 16)) if xcode else decode_table.get(entity, entity)
+	output=decode_regex.sub(substitute, input)
+	return output
+
+def graph(message):
+	pattern=re.compile("<reply><graph>(?P<graph>.+)</graph></reply>", re.DOTALL)
+	match=pattern.match(message)
+	if match:
+		graph=decode(match.group("graph"))
+		with open("graph.dot", "w+") as file:
+			file.write(graph)
+
+
+if len(sys.argv)>1:
+	if sys.argv[1]=="-p":
+		request="<action name=\"ping\"/>"
+	elif sys.argv[1]=="-th":
+		if len(sys.argv)>2:
+			request="<action name=\"state\"><option name=\"thread\">%s</option></action>"%sys.argv[2]
+	elif sys.argv[1]=="-ob":
+		if len(sys.argv)>2:
+			request="<action name=\"state\"><option name=\"object\">%s</option></action>"%sys.argv[2]
+	elif sys.argv[1]=="-ao":
+		request="<action name=\"analyse\"><option name=\"objects\"/></action>"
+	elif sys.argv[1]=="-ag":
+		request="<action name=\"analyse\"><option name=\"garbage\"/></action>"
+	elif sys.argv[1]=="-q":
+		if len(sys.argv)>2:
+			request="<action name=\"query\"><option name=\"objects\">%s</option></action>"%sys.argv[2]
+	elif sys.argv[1]=="-qr":
+		if len(sys.argv)>2:
+			request="<action name=\"query\"><option name=\"objects\">%s</option>" \
+				"<option name=\"referents\"/><option name=\"referrers\"/></action>"%sys.argv[2]
+	elif sys.argv[1]=="-g":
+		if len(sys.argv)>2:
+			request="<action name=\"query\"><option name=\"graph\">%s</option></action>"%sys.argv[2]
+			handler=graph
+	elif sys.argv[1]=="-a":
+		if len(sys.argv)>2:
+			address=sys.argv[2]
+
+
+sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(timeout)
+sock.connect((address, port))
+sock.send(request)
+message=""
+while True:
+	reading, writing, erratic=select.select((sock,), (), (), timeout)
+	if reading:
+		chunk=sock.recv(frame)
+		message+=chunk
+		if pattern.match(message):
+			break
+print message
+handler(message)
+
---
+++ bin/udpstate.py
@@ -0,0 +1,41 @@
+
+import sys, socket
+
+
+address="127.0.0.1"
+port=10101
+frame=64*1024
+timeout=1
+request="<action name=\"state\"/>"
+
+
+if len(sys.argv)>1:
+	if sys.argv[1]=="-p":
+		request="<action name=\"ping\"/>"
+	elif sys.argv[1]=="-th":
+		if len(sys.argv)>2:
+			request="<action name=\"state\"><option name=\"thread\">%s</option></action>"%sys.argv[2]
+	elif sys.argv[1]=="-ob":
+		if len(sys.argv)>2:
+			request="<action name=\"state\"><option name=\"object\">%s</option></action>"%sys.argv[2]
+	elif sys.argv[1]=="-ao":
+		request="<action name=\"analyse\"><option name=\"objects\"/></action>"
+	elif sys.argv[1]=="-ag":
+		request="<action name=\"analyse\"><option name=\"garbage\"/></action>"
+	elif sys.argv[1]=="-q":
+		if len(sys.argv)>2:
+			request="<action name=\"query\"><option name=\"objects\">%s</option></action>"%sys.argv[2]
+	elif sys.argv[1]=="-qr":
+		if len(sys.argv)>2:
+			request="<action name=\"query\"><option name=\"objects\">%s</option>" \
+				"<option name=\"referents\"/><option name=\"referrers\"/></action>"%sys.argv[2]
+	elif sys.argv[1]=="-a":
+		if len(sys.argv)>2:
+			address=sys.argv[2]
+
+
+sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(timeout)
+sock.sendto(request, (address, port))
+message, address=sock.recvfrom(frame)
+print message
