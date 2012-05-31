9dd5c210e2bd9e2416b62fa2674d3745b97b9c44
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2012-05-31 09:57:33 +0000

    Watcher: Utilities
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@8216 230e8929-42e3-3b44-a413-55659dd3a577

--- bin/tcpstate.py
+++ bin/tcpstate.py
@@ -45,10 +45,12 @@ if len(sys.argv)>1:
 	elif sys.argv[1]=="-q":
 		if len(sys.argv)>2:
 			request="<action name=\"query\"><option name=\"objects\">%s</option></action>"%sys.argv[2]
-	elif sys.argv[1]=="-qr":
+	elif sys.argv[1]=="-rr":
 		if len(sys.argv)>2:
-			request="<action name=\"query\"><option name=\"objects\">%s</option>" \
-				"<option name=\"referents\"/><option name=\"referrers\"/></action>"%sys.argv[2]
+			request="<action name=\"query\"><option name=\"referrers\">%s</option></action>"%sys.argv[2]
+	elif sys.argv[1]=="-rn":
+		if len(sys.argv)>2:
+			request="<action name=\"query\"><option name=\"referents\">%s</option></action>"%sys.argv[2]
 	elif sys.argv[1]=="-g":
 		if len(sys.argv)>2:
 			request="<action name=\"query\"><option name=\"graph\">%s</option></action>"%sys.argv[2]
