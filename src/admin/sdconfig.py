1d897fd739294ae75fa73014f624aaacae14dfd6
Pavel Kim <Pavel Kim@230e8929-42e3-3b44-a413-55659dd3a577>
2012-10-01 08:22:17 +0000

    again
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@8498 230e8929-42e3-3b44-a413-55659dd3a577

--- src/admin/sdconfig.py
+++ src/admin/sdconfig.py
@@ -223,7 +223,7 @@ def run(request):
 				if "backup_app[]" in args:
 					for appid in args["backup_app[]"]:
 						managers.backup_manager.backup(appid, args["devid"][0], args["rotation"][0])
-					request.write('<script language="javascript">parent.server.document.getElementById("MsgSvrInfo").innerHTML="%s backuped..";</script>'%args["backup_app[]"])
+					request.write('<script language="javascript">parent.server.document.getElementById("MsgSvrInfo").innerHTML="Backup passed good.";</script>')
 				else:
 					request.write('<script language="javascript">parent.server.document.getElementById("MsgSvrInfo").innerHTML="There are no applications to backup";</script>')
 			except Exception as e:
