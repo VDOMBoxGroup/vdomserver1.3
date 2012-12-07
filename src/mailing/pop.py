a1b4a3ae06ae0aeab15fbf459e24c927dc65f1a4
Michael Ovsyannikov <Michael Ovsyannikov@230e8929-42e3-3b44-a413-55659dd3a577>
2012-12-07 08:07:35 +0000

    smtp and pop3 for python and vscript
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@8642 230e8929-42e3-3b44-a413-55659dd3a577

---
+++ src/mailing/pop.py
@@ -0,0 +1,56 @@
+import poplib
+import time
+from .message import MailAttachment, Message
+class VDOM_Pop3_client(object):
+	def __init__(self, server,port=110, secure=False):
+		self.server = server
+		self.port = port
+		self.secure = secure
+		self.message_count=0
+		self.read_mails_count = 0
+		if self.secure:
+			self.connection = poplib.POP3_SSL(self.server, self.port)
+		else:
+			self.connection = poplib.POP3(self.server, self.port)		
+		self.connected = True
+		
+	def user(self, login, passw):
+		self.connection.user(login.encode('utf8'))
+		self.connection.pass_(passw.encode('utf8'))	
+		try:
+			self.message_count = self.connection.stat()[0]
+		except:
+			self.connected = False
+		
+	def __len__(self) :
+		"Return the number of messages at POP-server"
+		try:
+			self.message_count = self.connection.stat()[0]
+		except:
+			self.connected = False
+	
+	def quit(self):
+		self.connection.quit()	
+		
+	def fetch_message(self,id,delete=False):
+		if not self.connected:
+			return None
+		email_id = str(self.connection.uidl(id+1).split()[2])
+		email_size = str(self.connection.list(id+1).split(" ")[2])
+		email_content_decode = ""
+		email_content = '\n'.join(self.connection.retr(id+1)[1])
+		msg = Message.fromstring(email_content,email_id)
+		if delete:
+			self.connection.dele(id+1)
+		return msg
+	
+	def fetch_all_messages(self,offset=0,limit=None, delete=False):
+		if not self.connected:
+			return []
+		emails = []
+		mail_number = -1
+		for i in xrange(offset, limit or self.message_count):
+			mail_number = i+1
+			emails.append(self.fetch_message(i,delete))
+		self.read_mails_count = mail_number
+		return emails
