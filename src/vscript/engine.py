01b076e0f43ad14e2f79757ef57f963b42f0710e
Nikolay Grishkov <Nikolay Grishkov@230e8929-42e3-3b44-a413-55659dd3a577>
2012-12-10 10:19:06 +0000

    VScript: Add alternative names
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@8652 230e8929-42e3-3b44-a413-55659dd3a577

--- src/vscript/engine.py
+++ src/vscript/engine.py
@@ -44,6 +44,8 @@ vscript_default_environment={
 	u"v_wholeincorrectresponse": vscript_wrappers_name,
 	u"v_wholenoapierror": vscript_wrappers_name,
 	u"v_wholenoapplication": vscript_wrappers_name,
+	u"v_attachment": vscript_wrappers_name,
+	u"v_message": vscript_wrappers_name,
 	u"v_mailattachment": vscript_wrappers_name,
 	u"v_mailmessage": vscript_wrappers_name}
 
