422b8a355a69398a7be331895d2335ec9d6cf61e
Michael Ovsyannikov <Michael Ovsyannikov@230e8929-42e3-3b44-a413-55659dd3a577>
2012-12-19 06:37:06 +0000

    for backward compatibility aliases for some whole errors were added
    git-svn-id: http://svn.vdombox.ru/VDOMServer/branch/VDOM_current@8668 230e8929-42e3-3b44-a413-55659dd3a577

--- src/vscript/wrappers/__init__.py
+++ src/vscript/wrappers/__init__.py
@@ -13,4 +13,7 @@ from .vdombox import v_vdombox
 from .whole import v_wholeconnection, v_wholeapplication, \
 	v_wholeerror, v_wholeconnectionerror, v_wholenoconnectionerror, \
 	v_wholeremotecallerror, v_wholeincorrectresponseerror, \
-	v_wholenoapierror, v_wholenoapplicationerror
+	v_wholenoapierror, v_wholenoapplicationerror,\
+        v_wholeincorrectresponseerror as v_wholeincorrectresponse, \
+	v_wholenoapplicationerror as v_wholenoapplication
+
