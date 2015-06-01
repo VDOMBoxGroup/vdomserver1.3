d688543db988c91ecc776079e3f29552bf0bdc25
SergejPetrov <sergejpetrov@macbook-petrov.tomcity.net>
2015-06-01 17:55:48 +0600

    - Added: dynamic objects ver.0.0.1 (: - Added: e2vdom JS code generator for dynamic objects. Support objects without external libraries

--- src/e2vdom/compiler.py
+++ src/e2vdom/compiler.py
@@ -2,102 +2,140 @@
 import managers
 
 
-def compile_declarations_n_libraries(container):
-	#lines, objects, types, containers, libraries=[], {}, {}, {}, {}
-	lines, types, containers, libraries=[], container.object.types, container.object.containers, container.object.libraries
-	#collect_information(container.object, objects, types, containers, libraries)
 
+def vdom_type_to_js(vdomtype, output):
+	"""
+	Render VDOM Type to JavaScript code
+	"""
+	events = vdomtype.event_info.user_interface_events + vdomtype.event_info.object_events.keys()
+	events = "this.registerEvents([%s])" % ", ".join(["\"%s\"" % event for event in events]) if events else ""
+
+	output(
+	    ("function VDOM_Type_%(type)s(id, eventEngine){\n"
+	      "  this.Base=VDOM_Object;\n"
+	      "  this.Base(id, eventEngine);"
+	      "  %(events)s"
+	      "};\n"
+	      "VDOM_Type_%(type)s.prototype=new VDOM_Object;") % {
+	        "type": vdomtype.id.replace("-", "_"), 
+	        "events": events
+	    }
+	)
+
+	actions = vdomtype.action_info.get("2330fe83-8cd6-4ed5-907d-11874e7ebcf4", None) # TODO!!!
+	if actions:
+		for name, action in actions.iteritems():
+			output(
+			    "VDOM_Type_%(type)s.prototype.%(name)s=function(%(parameters)s){%(source)s}" % {
+			        "type": vdomtype.id.replace("-", "_"),
+			        "name": name,
+			        "parameters": ("".join([", %s" %  parameter.name for parameter in action.parameters]).replace('"',r'\"') )[2:],
+			        "source": action.source_code
+			    }
+			)
+
+	return output
+
+
+def compile_declarations_n_libraries(container):
+
+	types = container.object.types
+	containers = container.object.containers
+	libraries = container.object.libraries
+	
 	libraries="".join(["".join(library) for library in libraries.itervalues()])
 	libraries+="".join(["".join(library) for library in managers.request_manager.current.dyn_libraries.itervalues()])
 	
-
-#	lines.append("jQuery(document).ready(function($){")
-
-	lines.append("EventQueue=new VDOM_EventQueue;")
+	lines = ["EventQueue=new VDOM_EventQueue;"]
+	append_lines = lines.append
 	
-	for id, type in containers.iteritems():
-		lines.append(
+	for type_id in containers.iterkeys():
+		append_lines(
 			"function VDOM_Obj_Type_%(type)s(id, eventEngine){this.Base=VDOM_Object;this.Base(id, eventEngine)};\n"\
-			"VDOM_Obj_Type_%(type)s.prototype=new VDOM_Object;"%\
-			{"type": id.replace("-", "_")})
+			"VDOM_Obj_Type_%(type)s.prototype=new VDOM_Object;" % {
+		        "type": type_id.replace("-", "_")
+		    }
+		)
 	
-	for id, type in types.iteritems():
-		events=type.event_info.user_interface_events+type.event_info.object_events.keys()
-		events=";this.registerEvents([%s])"%", ".join(["\"%s\""%event for event in events]) if events else ""
-		lines.append(
-			"function VDOM_Type_%(type)s(id, eventEngine){this.Base=VDOM_Object;this.Base(id, eventEngine)%(events)s};\n"\
-			"VDOM_Type_%(type)s.prototype=new VDOM_Object;"%\
-			{"type": id.replace("-", "_"), "events": events})
-		actions=type.action_info.get("2330fe83-8cd6-4ed5-907d-11874e7ebcf4", None) # TODO!!!
-		if actions:
-			for name, action in actions.iteritems():
-				lines.append(
-					"VDOM_Type_%(type)s.prototype.%(name)s=function(%(parameters)s){%(source)s}"%\
-					{"type": id.replace("-", "_"), "name": name,
-					"parameters": ("".join([", %s"%parameter.name for parameter in action.parameters]).replace('"',r'\"') )[2:],
-					"source": action.source_code})
-	
-#	lines.append("});")
-
+	for type_id, type_obj in types.iteritems():
+		vdom_type_to_js(type_obj, append_lines)
+		
 	return "\n".join(lines), libraries
 
+
 def compile_registations_render_params(parameters):
-	a = []
+	out = []
+	
 	for parameter in parameters:
-		x = parameter.value.replace(r'\"', '"')
-		if len(x) > 0:
-			if x[0] == '"':
-				a.append( r'\"' + x[1:-1].replace('"', r'&quot;') + r'\"' )
+		
+		value = parameter.value.replace(r'\"', '"')
+		if len(value) > 0:
+			
+			if value[0] == '"':
+				out.append(r'\"' + value[1:-1].replace('"', r'&quot;') + r'\"')
+			
 			else:
-				a.append( x.replace('"', r'\"') )
-	return ','.join(a)
+				out.append(value.replace('"', r'\"'))
+				
+	return ','.join(out)
+
 
 def compile_registations(container, parent):
 
-	application=managers.request_manager.get_request().application()
+	application = managers.request_manager.get_request().application()
 	if container.id in application.all_dynamic_objects:
 		return ""
 
-	lines=[]
+	lines = []
+	lines_append = lines.append
+	
+	lines_append(
+		"if(typeof Obj_%(container_id)s_EventEngine!=='undefined'){Obj_%(container_id)s_EventEngine.stop();delete Obj_%(container_id)s_EventEngine;}\n"\
+		"if(typeof Obj_%(container_id)s_Dispatcher!=='undefined'){delete Obj_%(container_id)s_Dispatcher;}\n"\
+		"Obj_%(container_id)s_Dispatcher=new VDOM_EventDispatcher();\n"\
+		"Obj_%(container_id)s_EventEngine=new VDOM_EventEngine(Obj_%(container_id)s_Dispatcher, EventQueue);" % {
+	        "container_id": container.id.replace("-", "_")
+	    }
+	)
 
-	#lines.append(";jQuery(document).ready(function($){")
-
-	lines.append(
-		"if(typeof Obj_%(container)s_EventEngine!=='undefined'){Obj_%(container)s_EventEngine.stop();delete Obj_%(container)s_EventEngine;}\n"\
-		"if(typeof Obj_%(container)s_Dispatcher!=='undefined'){delete Obj_%(container)s_Dispatcher;}\n"\
-		"Obj_%(container)s_Dispatcher=new VDOM_EventDispatcher();\n"\
-		"Obj_%(container)s_EventEngine=new VDOM_EventEngine(Obj_%(container)s_Dispatcher, EventQueue);"%\
-		{"container": container.id.replace("-", "_")})
-
-	lines.append(
-		"function VDOM_Obj_Type_%(type)s(id, eventEngine){this.Base=VDOM_Object;this.Base(id, eventEngine)};\n"\
-		"VDOM_Obj_Type_%(type)s.prototype=new VDOM_Object;"%\
-		{"type": container.id.replace("-", "_")})
-
-	application=managers.request_manager.get_request().application()
-	#for source_object in [container.object.get_objects(), container.object]:
-	aa = container.object.get_objects()
-	aa[container.id] = container.object
-	for source_object in aa:
+	lines_append(
+		"function VDOM_Obj_Type_%(type_id)s(id, eventEngine){this.Base=VDOM_Object;this.Base(id, eventEngine)};\n"\
+		"VDOM_Obj_Type_%(type_id)s.prototype=new VDOM_Object;" % {
+	        "type_id": container.id.replace("-", "_")
+	    }
+	)
+	
+	# objects - dictionary of objects, key is object ID
+	objects = container.object.get_objects()
+	objects[container.id] = container.object
+	
+	for object_id in objects:
 		#print "[E2VDOM] Container", container.id
-		events=application.events_by_object.get(source_object, None)
+		events = application.events_by_object.get(object_id, None)
 		#print "[E2VDOM] Events", events
+		
 		if events:
 			for event in events.itervalues():
 				#print "[E2VDOM] Event", event.name
+				
 				for action_id in event.actions:
 					#print "[E2VDOM] Action", action_id
 					#print "[E2VDOM] Application actions", application.actions
 					#print "[E2VDOM] Event container", event.container
-					action=application.actions.get(action_id, None)
+					action = application.actions.get(action_id, None)
 					#print "[E2VDOM] Action", action
-					source="\"o_%s:%s\""%(source_object.replace("-", "_"), event.name)
+					source = "\"o_%s:%s\"" % (
+					    object_id.replace("-", "_"),
+					    event.name
+					)
+					
 					if action is None:
 						# action=application.search_object(event.container).actions["id"].get(action_id, None)
 						action, action_container=application.search_server_action(action_id)
 						#print "[E2VDOM] Action", action
 						if action is None:
-							target="\"ACHTUNG! UNKNOWN ACTION %s\""%(action_id)
+							target = "\"ACHTUNG! UNKNOWN ACTION %s\""%(action_id)
+
 						else:
 							target="\"server\""
 							#if action.target_object in container.object.objects:
@@ -107,39 +145,128 @@ def compile_registations(container, parent):
 							#	# schedule dispatch
 					else:
 						#if action.target_object in container.object.objects:
-						target="\"Obj_%s:%s(%s)\"" % ( action.target_object.replace("-", "_"), action.method_name,
+						target = "\"Obj_%s:%s(%s)\"" % (
+						    action.target_object.replace("-", "_"),
+						    action.method_name,
 							#", ".join([parameter.value.replace('"',r'\"') for parameter in action.parameters]) )
-							compile_registations_render_params(action.parameters) )
+							compile_registations_render_params(action.parameters)
+						)
 						#print compile_registations_render_params(action.parameters)
 						#else:
 						#	target="\'Obj_%s:bubbleEvent(\"%s\")\'"%(container.id.replace("-", "_"), action.method_name)
-					lines.append(
-						"Obj_%(container)s_Dispatcher.addDispatchEvent(%(source)s, %(target)s);"%\
-						{"container": container.id.replace("-", "_"), "source": source, "target": target})
+					lines_append(
+						"Obj_%(container)s_Dispatcher.addDispatchEvent(%(source)s, %(target)s);" % {
+					        "container": container.id.replace("-", "_"),
+					        "source": source,
+					        "target": target
+					    }
+					)
 
-
-	for object in container.object.get_objects_list():
-		lines.append(
-			"Obj_%(object)s=new VDOM_Type_%(object_type)s(\"o_%(object)s\", Obj_%(container)s_EventEngine);"%\
-			{"container": container.id.replace("-", "_"),
-			"object": object.id.replace("-", "_"), "object_type": object.type.id.replace("-", "_")})
+	for obj in container.object.get_objects_list():
+		lines_append(
+			"Obj_%(object)s=new VDOM_Type_%(object_type)s(\"o_%(object)s\", Obj_%(container)s_EventEngine);" % {
+		        "container": container.id.replace("-", "_"),
+		        "object": obj.id.replace("-", "_"),
+		        "object_type": obj.type.id.replace("-", "_")
+		    }
+		)
 
 	#print "[E2VDOM] Container %s, parent %s"%(container.id, parent)
 	#lines.append(
 	#	"Obj_%(container)s=new VDOM_Obj_Type_%(type)s(\"o_%(container)s\", Obj_%(container)s_EventEngine);"%\
 	#	{"container": container.id.replace("-", "_"), "type": container.object.type.id.replace("-", "_")})
-	lines.append( # Leo
-		"Obj_%(container)s=new VDOM_Type_%(type)s(\"o_%(container)s\", Obj_%(container)s_EventEngine);"%\
-		{"container": container.id.replace("-", "_"), "type": container.object.type.id.replace("-", "_")})
+	lines_append( # Leo
+		"Obj_%(container)s=new VDOM_Type_%(type)s(\"o_%(container)s\", Obj_%(container)s_EventEngine);" % {
+	        "container": container.id.replace("-", "_"),
+	        "type": container.object.type.id.replace("-", "_")
+	    }
+	)
+	
 	if parent:
-		lines.append(
+		lines_append(
 			"Obj_%(container)s.bubbleEvent=function(e){"\
-			"Obj_%(container)s.riseEvent('tfrEvent', e.parameters, Obj_%(parent)s_eventEngine);};"%\
-			{"container": container.id.replace("-", "_"), "parent": parent.replace("-", "_")})
-	lines.append(
-		"Obj_%(container)s_EventEngine.start();"%\
-		{"container": container.id.replace("-", "_")})
+			"Obj_%(container)s.riseEvent('tfrEvent', e.parameters, Obj_%(parent)s_eventEngine);};" % {
+		        "container": container.id.replace("-", "_"),
+		        "parent": parent.replace("-", "_")
+		    }
+		)
+	lines_append(
+		"Obj_%(container)s_EventEngine.start();" % {
+	        "container": container.id.replace("-", "_")
+	    }
+	)
 
 	#lines.append("});")
 
+	return "\n".join(lines)
+
+
+def compile_dynamic_e2vdom(container, actions=None):
+	"""
+	Render e2vdom code for dynamic/dummy objects
+	"""
+	application = managers.request_manager.get_request().application()
+	
+	lines = []
+	append_lines = lines.append
+
+	for type_id, type_obj in container.all_types().iteritems():
+		append_lines("if (!('VDOM_Type_%s' in window)){" % type_id.replace("-", "_"))
+		vdom_type_to_js(type_obj, append_lines)
+		append_lines("window.VDOM_Type_%(type)s = VDOM_Type_%(type)s;}" % {'type': type_id.replace("-", "_")})
+
+	append_lines(
+	    "if(typeof Obj_%(container_id)s_EventEngine!=='undefined'){Obj_%(container_id)s_EventEngine.stop();delete Obj_%(container_id)s_EventEngine;}\n"\
+	    "if(typeof Obj_%(container_id)s_Dispatcher!=='undefined'){delete Obj_%(container_id)s_Dispatcher;}\n"\
+	    "Obj_%(container_id)s_Dispatcher=new VDOM_EventDispatcher();\n"\
+	    "Obj_%(container_id)s_EventEngine=new VDOM_EventEngine(Obj_%(container_id)s_Dispatcher, EventQueue);" % {
+	        "container_id": container.id.replace("-", "_")
+	    }
+	)		
+	
+	if actions:
+		# actions is dictionary like
+		# {
+		#   (src_obj, event): [(dst_obj, action, param1, param2, param3)]
+		# }
+		def parse_params(params):
+			out = []
+			for value in params:
+				if value and value[0] == '"':
+					value = r'\"%s\"' % value[1:-1].replace('"', r'&quot;')
+		
+				else:
+					value = value.replace('"', r'\"')
+					
+				out.append(value)
+
+			return ','.join(out)
+	
+		
+		for event, actions in actions.iteritems():
+			for action in actions:
+				append_lines(
+					"Obj_%s_Dispatcher.addDispatchEvent(\"o_%s:%s\", \"Obj_%s:%s(%s)\");" % (
+					    container.id.replace("-", "_"),
+					    event[0].replace("-", "_"),
+					    event[1],
+					    action[0].replace("-", "_"),
+					    action[1],
+				        parse_params(action[2:])
+				    )
+				)
+		
+	for obj in container.all_objects().itervalues():
+		append_lines(
+		    "Obj_%(object)s=new window.VDOM_Type_%(object_type)s(\"o_%(object)s\", Obj_%(container)s_EventEngine);" % {
+		        "container": container.id.replace("-", "_"),
+		        "object": obj.id.replace("-", "_"),
+		        "object_type": obj.type.id.replace("-", "_")
+		    }
+		)
+	
+	append_lines(
+	    "Obj_%s_EventEngine.start();" % container.id.replace("-", "_")
+	)
+	
 	return "\n".join(lines)
---
+++ src/memory/dynobject.py
@@ -0,0 +1,280 @@
+
+import e2vdom
+import managers
+import utils.uuid as uuid
+
+
+class DummyAttribute(object):
+
+    __slots__ = (
+        "name",
+        "value"
+    )
+
+    def __init__(self, name, value=0):
+        self.name = name
+        self.value = value
+        
+    def __repr__(self):
+        return self.value
+
+
+class DummyAttributes(object):
+
+    __slots__ = ("attrs",)
+
+    def __init__(self):
+        self.attrs = {}
+
+    def __getitem__(self, key):
+        if key == "name":
+            return DummyAttribute("name", "")
+
+        elif key == "dynamic":
+            return DummyAttribute("dynamic",1)
+
+        if key not in self.attrs:
+            self.attrs[key] = DummyAttribute(key)
+
+        return self.attrs[key]
+
+    def values(self):
+        return self.attrs.values()
+
+
+class DummyObject(object):
+
+    def __init__(self, vdomtype, name, action_name):
+        self.name = name
+        self.actions = {
+            "name": {}, 
+            "id": {}
+        }
+        self.attributes = DummyAttributes()
+        self.containers = {}
+        self.dynamic = {(action_name, self.name): 1}
+        self.id = str(uuid.uuid4())
+        self.libraries = {}
+        self.objects = {}
+        self.objects_list = []
+        self.objects_by_name = {self.name: self}
+        self.order = 0
+        self.type = vdomtype
+        self.types = {self.type.id: self.type}
+        
+    def __repr__(self):
+        return "VDOM Dyn Object(<%s name='%s'> %r )" % (
+            self.type.name,
+            self.name,
+            self.attributes.attrs
+        )
+
+    def get_attributes(self):
+        return self.attributes
+
+    def set_attribute_ex(self, name, value, do_compute=True):
+        old_value = self.attributes[name].value
+        self.attributes[name].value = value
+        return {
+            "value": value,
+            "old_value": old_value
+        }        
+
+    def get_objects_list(self):
+        return self.objects_list
+
+    def get_objects(self):
+        return self.objects
+
+    def add_child(self, child):
+        self.objects_by_name[child.name] = child
+        self.objects_list.append(child)
+        self.objects[child.id] = child
+        self.types[child.type.id] = child.type
+        
+    def regenerate_uuid(self, recursive=False):
+        """
+        Create new guids, if needed - recursively
+        """
+        self.id = str(uuid.uuid4())
+        if not recursive:
+            return
+        
+        all_objects = {self.id: self}
+        self.objects = {}
+        
+        for child in self.objects_list:
+            all_objects.update(child.regenerate_uuid(recursive=recursive))
+            self.objects[child.id] = child
+            
+        return all_objects
+    
+    def all_objects(self):
+        """
+        Return dictionary with all objects
+        """
+        all_objects = {self.id: self}
+        for child in self.objects_list:
+            all_objects.update(child.all_objects())
+            
+        return all_objects
+    
+    def find_child_by_name(self, childname):
+        """
+        Find child element by name
+        """
+        child = self
+        for cname in childname.split("."):
+            child = child.objects_by_name.get(cname, None)
+            if not child:
+                return None
+        
+        return child
+                    
+    def all_types(self):
+        """
+        Return all child types
+        """
+        types = self.types.copy()
+        for child in self.objects.itervalues():
+            types.update(child.all_types())
+            
+        
+        return types
+            
+
+def create_dummy_objects(vdomxml, parent=None):
+    """
+    Create set of dummy objects
+    @vdomxml must have root object (container or something else) i.e.:
+    <container>
+      <text />
+      <text />
+    </container>
+    
+    or
+    
+    <container>
+      <container>
+        <text />
+      </container>
+      <container>
+        <text />
+      </container>
+    </container>
+    """
+    
+    objects = {}
+    attr_map = {}
+    
+    type_obj = managers.xml_manager.get_type_by_name(vdomxml.lname)
+    
+    for attr_name in type_obj.get_attributes():
+        attr_map[attr_name.lower()] = type_obj.get_attributes()[attr_name].default_value
+
+    # parse attributes
+    for attr_name in vdomxml.attributes:
+        if attr_name in attr_map or attr_name == "name":
+            attr_map[attr_name] = vdomxml.attributes[attr_name]
+
+    for child in vdomxml.children:
+        if child.lname == "attribute":
+            attr_name = child.attributes["name"]
+
+            if attr_name and attr_name in attr_map:
+                attr_map[attr_name] = child.get_value_as_xml()
+
+    dummy_obj = DummyObject(type_obj, attr_map["name"], e2vdom.virtual_context)
+    objects[dummy_obj.id] = dummy_obj
+    
+    for key, value in attr_map.items():
+        dummy_obj.set_attribute_ex(key, value)
+        
+    if parent:
+        parent.add_child(dummy_obj)
+
+    # walk through childs
+    for child in vdomxml.children:
+        if "attribute" == child.lname:
+            continue
+
+        child, child_objects = create_dummy_objects(child, dummy_obj)
+        objects.update(child_objects)
+
+    return dummy_obj, objects
+
+
+def update_attributes(parent, attributes):
+    """
+    Update objects attributes recursively
+    """
+    cache = {}
+    old_values = {}
+
+    for attr, value in attributes.iteritems():
+        # split key by '.'
+        # last item in list 
+        #   is target attribute name
+        splitted_attr = attr.rsplit(".", 1)
+        
+        obj = None
+        if len(splitted_attr) == 1:
+            obj = parent
+            
+        elif splitted_attr[0] in cache:
+            obj = cache[splitted_attr[0]]
+            
+        else:
+            obj = parent
+            for obj_name in splitted_attr[0].split("."):
+                obj = obj.objects_by_name.get(obj_name, None)
+                if not obj:
+                    break
+            
+            else:    
+                cache[splitted_attr[0]] = obj
+            
+        if obj:
+            ret = obj.set_attribute_ex(splitted_attr[-1], value)
+            old_values[attr] = ret['old_value']
+    
+    return old_values
+            
+ 
+def dynamic_render(dummy_object, objects_dict, parent=None):
+    """
+    Render dummy objects
+    """
+    application = managers.request_manager.get_request().application()
+    
+    for obj_id, obj in objects_dict.iteritems():
+        application.all_dynamic_objects[obj_id] = obj
+        
+    result = managers.engine.render(
+        application,
+        dummy_object,
+        parent,
+        "vdom"
+    )
+    
+    for obj_id, obj in objects_dict.iteritems():
+        application.all_dynamic_objects.pop(obj_id, None)
+    
+    return result
+
+
+def dummy_e2vdom(objects_dict):
+    """
+    Render dummy e2vdom objects
+    """
+    if not objects_dict:
+        return ""
+    
+    out = ['<script>']
+    for obj_id in objects_dict:
+        out.append(
+            '''var Obj_%s = {"eventEngine": {"eventDispatcher": {"eventArray": []}}};''' % obj_id.replace("-", "_")
+        )
+    out.append('</script>')
+    
+    return "\n".join(out)
