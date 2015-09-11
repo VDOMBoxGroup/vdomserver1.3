e7fad7ad5c0a6c9d66a7293a5fdac06592eb7b4f
Mikhail <mich_ovs@mail.ru>
2015-09-12 00:23:59 +0600

    * htmlcontainer - fixed bug with ui-overlay position * resapi - auth replaced with target container name check(only API alowed)

--- src/admin/restapi.py
+++ src/admin/restapi.py
@@ -7,24 +7,24 @@ from utils.semaphore import VDOM_semaphore
 def run(request):
 	args = request.arguments().arguments()
 	request.render_type = "e2vdom"
-	auth = request.headers().headers().get("Authorization")
-	if not auth:
-		request.add_header("WWW-Authenticate","Basic realm=\"vdom\"")
-		request.send_htmlcode(401)
+	#auth = request.headers().headers().get("Authorization")
+	#if not auth:
+		#request.add_header("WWW-Authenticate","Basic realm=\"vdom\"")
+		#request.send_htmlcode(401)
 		
-		return
-	else:
-		if auth[:len("Basic ")]=="Basic ":
-			user,login = base64.b64decode(auth[len("Basic "):]).split(":")
-			sem = VDOM_semaphore()
-			sem.lock()
-			try:
-				if not managers.user_manager.match_user_md5(user, hashlib.md5(login).hexdigest()):
-					time.sleep(1)
-					request.send_htmlcode(401)
-					return
-			finally:
-				sem.unlock()			
+		#return
+	#else:
+		#if auth[:len("Basic ")]=="Basic ":
+			#user,login = base64.b64decode(auth[len("Basic "):]).split(":")
+			#sem = VDOM_semaphore()
+			#sem.lock()
+			#try:
+				#if not managers.user_manager.match_user_md5(user, hashlib.md5(login).hexdigest()):
+					#time.sleep(1)
+					#request.send_htmlcode(401)
+					#return
+			#finally:
+				#sem.unlock()			
 	appid = args.get("appid")[0] if args.get("appid") else ""
 	container = args.get("objid")[0] if args.get("objid") else ""
 	action =args.get("action_name")[0] if args.get("action_name") else ""
@@ -35,7 +35,12 @@ def run(request):
 		request.write("<ERROR>Invalid params</ERROR>")
 	else:
 		try:
-			ret = managers.dispatcher.dispatch_action(appid, container, action, xml_param,xml_data)
+			app = managers.xml_manager.get_application(appid)
+			obj = app.search_object(container)
+			if not obj or obj.name.lower() != "api":
+				request.write("<ERROR>Invalid params</ERROR>")
+			else:
+				ret = managers.dispatcher.dispatch_action(appid, container, action, xml_param,xml_data)
 		except Exception as e:
 			request.write("<ERROR>%s</ERROR>"%e)
 		else:
