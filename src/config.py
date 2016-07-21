16d58fabcb8051bf1ce2b30177aeaa603417e5d8
Sergey.Sysoev <Sergey.Sysoev@vdombox.ru>
2016-07-21 08:46:44 +0000

    evalstring.py

--- src/config.py
+++ src/config.py
@@ -1,26 +1,27 @@
 VDOM_CONFIG = {
-	"SERVER-ID"				: "5ED67D80-9017-4753-9633-685A1926A6B9",
+	"SERVER-ID"                             : "5ED67D80-9017-4753-9633-685A1926A6B9",
 	"DEFAULT-LANGUAGE"			: "en",
 	"HTTP-ERROR-PAGES-DIRECTORY"		: "../errors",
 
 	# managers directories
-	"FILE-ACCESS-DIRECTORY"			: "../app",
-	"XML-MANAGER-DIRECTORY"			: "../app",
-	"APPLICATION-XML-TEMPLATE"		: "../app/app_template.xml",
-	"SOURCE-MODULES-DIRECTORY"		: "../app/objects",
+	"FILE-ACCESS-DIRECTORY"			: "/var/vdom",
+	"XML-MANAGER-DIRECTORY"			: "/var/vdom",
+	"APPLICATION-XML-TEMPLATE"		: "/var/vdom/app_template.xml",
+	"SOURCE-MODULES-DIRECTORY"		: "/var/vdom/objects",
+	"SERVER-INFORMATION-DIRECTORY"		: "/var/spool/vdom/",
 
 	# server stuff
 	"SERVER-ADDRESS"			: "",
 	"SERVER-PORT"				: 80,
-	"SERVER-DAV-PORT"			: 8008,
-	"SECURE-SERVER-PORT"			: 443,
 	"SERVER-LOCALHOST-PORT"			: 2222,
+	"VDOM-MEMORY-SERVER-PORT"		: 3333,
 	"LOCALHOST-CARD-PORT"			: 4444,
 	"LOCALHOST-LOGGER-PORT"			: 5555,
-	"WATCHER-PORT"				: 10101,
-	"SERVER-PIDFILE"			: "../app/server.pid",
-	"LOGGER-PIDFILE"			: "../app/logger.pid",
-        "SERVER-INFORMATION-DIRECTORY"          : "../app/spool",
+	"SECURE-SERVER-PORT"			: 443,
+	"SERVER-DAV-PORT"			: 8008,
+	"WATCHER-PORT"				:10101,
+	"SERVER-PIDFILE"			: "/var/vdom/server.pid",
+	"LOGGER-PIDFILE"			: "/var/vdom/logger.pid",
 	"SERVER-SOURCE-MANAGER-MEMORY-QUOTE"	: "10240",
 	"AUTO-REMOVE-INCORRECT-APPLICATIONS"	: 0,
 
@@ -28,10 +29,10 @@ VDOM_CONFIG = {
 	"SOAP-POST-URL"				: "/SOAP",
 	"MANAGEMENT-URL"			: "/system",
 	"WSDL-FILE-URL"				: "/vdom.wsdl",
-	"WSDL-FILE-LOCATION"			: "../app/vdom.wsdl",
+	"WSDL-FILE-LOCATION"			: "/var/vdom/vdom.wsdl",
 
 	"SOURCE-TYPES-LOCATION"			: "../types",
-	"TYPES-LOCATION"			: "../app/types",
+	"TYPES-LOCATION"			: "/var/vdom/types",
 
 	# log
 	"LOG-FILE-SIZE"				: 500000,		# size of one log file
@@ -46,24 +47,24 @@ VDOM_CONFIG = {
 	"WYSIWYG-TIMEOUT"			: 30.1,
 	"APP-SAVE-TIMEOUT"			: 30.1,
 
-	"STORAGE-DIRECTORY"			: "../app",
-	"TEMP-DIRECTORY"			: "../app/temp",	# must be without spaces!
+	"STORAGE-DIRECTORY"			: "/var/vdom",
+	"TEMP-DIRECTORY"			: "/var/vdom/temp",	# must be without spaces!
 	"FONT-DIRECTORY"			: "../fonts",
-	"BACKUP-DIRECTORY"			: "../app/backup",
-	"SHARE-DIRECTORY"			: "../app/share",
-	"FILE-STORAGE-DIRECTORY"		: "../app/storage",
-	"LIB-DIRECTORY"				: "../app/lib",
-	"LOG-DIRECTORY"				: "../app/log",
-	
+	"BACKUP-DIRECTORY"			: "/var/vdom/backup",
+	"SHARE-DIRECTORY"			: "/var/vdom/share",
+	"FILE-STORAGE-DIRECTORY"	: "/var/vdom/storage",
+	"LIB-DIRECTORY"				: "/var/vdom/lib",
+	"LOG-DIRECTORY"				: "/var/vdom/log",
+
 	# storage keys
 	"XML-MANAGER-TYPE-STORAGE-RECORD"	: "XML_TYPE_DATA",
 	"XML-MANAGER-APP-STORAGE-RECORD"	: "XML_APPLICATION_DATA",
 	"VIRTUAL-HOSTING-STORAGE-RECORD"	: "VIRTUAL_HOSTING_DATA",
-	"FILE-MANAGER-INDEX-STORAGE-RECORD"	: "STORAGE_FILE_INDEX",
+	"FILE-MANAGER-INDEX-STORAGE-RECORD"     : "STORAGE_FILE_INDEX",
 	"RESOURCE-MANAGER-INDEX-STORAGE-RECORD"	: "RESOURCE_FILE_INDEX",
 	"DATABASE-MANAGER-INDEX-STORAGE-RECORD"	: "DATABASE_FILE_INDEX",
 	"SCHEDULER-MANAGER-INDEX-STORAGE-RECORD": "SCHEDULER_INDEX",
-	"BACKUP-STORAGE-DRIVER-INDEX-RECORD"	: "STORAGE_DRIVER_INDEX",
+	"BACKUP-STORAGE-DRIVER-INDEX-RECORD"    : "STORAGE_DRIVER_INDEX",
 	"SOURCE-SWAP-FILE-INDEX-STORAGE-RECORD" : "SWAP_FILE_INDEX",
 	"USER-MANAGER-STORAGE-RECORD" 		: "USER_INFO_DATA",
 	"USER-MANAGER-ROOT-ID-STORAGE-RECORD" 	: "ROOT_USER_ID",
@@ -75,7 +76,7 @@ VDOM_CONFIG = {
 	# vscript
 	"DISABLE-VSCRIPT": 0,
 	"LAZY-MODE": 0,
-	"PRELICENSE":{},        
+	"PRELICENSE":{},
 }
 
 VDOM_CONFIG_1 = {
@@ -87,14 +88,13 @@ VDOM_CONFIG_1 = {
 	"SMTP-SENDMAIL-TIMEOUT"			: 20.0,
 	"SMTP-SERVER-ADDRESS"			: "smtp.gmail.com",
 	"SMTP-SERVER-PORT"			: 465,
-	"SMTP-SERVER-USER"			: "Vdom.Server@gmail.com",
-	"SMTP-SERVER-PASSWORD"			: "zdupxsbnkbllgtox",
+	"SMTP-SERVER-USER"			: "vdom.server@gmail.com",
+	"SMTP-SERVER-PASSWORD"			: "VDMNK22YK",
 	"SMTP-SERVER-SENDER"			: "",
 	"SMTP-OVER-SSL"				: 1,
 
 	# security
 	"ROOT-PASSWORD"				: "root",
-	
-	# SSL Server
-	"ENABLE-SSL"	:		"0",
+	"ENABLE-SSL"				: "0",
+
 }
